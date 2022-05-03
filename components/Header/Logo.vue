c8ea5744c106f3b7678306571739e8d6b6c62a63
cuzknothz <cuzknothz@gmail.com>
2022-05-03 21:40:47 +0700

    fix position logo

--- components/Header/Logo.vue
+++ components/Header/Logo.vue
@@ -3,7 +3,7 @@
 <template>
   <img
     alt="Instagram"
-    class="h-[29px] cursor-pointer"
+    class="mt-[7px] h-[29px] cursor-pointer"
     src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
   />
 </template>
