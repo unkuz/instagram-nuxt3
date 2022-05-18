b1276c73dfb6c597985f0fc4d6d2e42abf25ad7c
cuzknothz <cuzknothz@gmail.com>
2022-05-18 23:58:43 +0700

    _

--- pages/explore.vue
+++ pages/explore.vue
@@ -2,36 +2,43 @@
 
 <template>
   <div class="grid h-[500px] w-full grid-cols-3 gap-[2px] md:gap-[28px]">
-    <div class="aspect-square h-full w-full overflow-hidden bg-fuchsia-300">
+    <div class="aspect-square overflow-hidden bg-fuchsia-300">
       <img
+        class="aspect-square w-full object-cover"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
 
-    <div
-      class="aspect-square w-full bg-fuchsia-300 md:col-start-1 md:row-start-2"
-    >
-      <img src="/explore/278009228_960257008015789_1126924171986179644_n.jpg" />
+    <div class="aspect-square bg-fuchsia-300 md:col-start-1 md:row-start-2">
+      <img
+        class="aspect-square w-full object-cover"
+        src="/explore/278009228_960257008015789_1126924171986179644_n.jpg"
+      />
     </div>
     <div
-      class="col-start-3 row-span-2 row-start-1 h-full w-full bg-fuchsia-300 object-cover md:col-span-2 md:col-start-2 md:aspect-square"
+      class="col-start-3 row-span-2 row-start-1 bg-fuchsia-300 object-cover md:col-span-2 md:col-start-2"
     >
       <img
-        class="h-full object-cover md:h-auto"
+        class="aspect-auto h-full w-full object-cover md:aspect-square"
         src="/explore/275624700_382008663748898_3572544032225631298_n.jpg"
       />
     </div>
 
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square bg-fuchsia-300">
       <img
+        class="aspect-square w-full object-cover"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
-      <img src="/explore/275624700_382008663748898_3572544032225631298_n.jpg" />
-    </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square bg-fuchsia-300">
       <img
+        class="aspect-square w-full object-cover"
+        src="/explore/275624700_382008663748898_3572544032225631298_n.jpg"
+      />
+    </div>
+    <div class="aspect-square bg-fuchsia-300">
+      <img
+        class="aspect-auto w-full object-cover md:aspect-square"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
