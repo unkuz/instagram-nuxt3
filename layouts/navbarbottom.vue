fc09031020866a3ba3beaf08eeb606061a014c85
cuzknothz <cuzknothz@gmail.com>
2022-05-18 23:45:32 +0700

    🍕🍕🍕🍕

--- layouts/navbarbottom.vue
+++ layouts/navbarbottom.vue
@@ -7,13 +7,15 @@ import { useSectionStore } from "~~/store/section";
 import Reels from "../components/Nav/Reels.vue";
 import { SECTION } from "~~/constants/section";
 const { width } = useResizeWindow();
+const router = useRouter();
+const isMobileResponsive = computed(() => width.value < 768);
 
 const sectionStore = useSectionStore();
 const section = computed(() => sectionStore.getSectionSelect);
-const handleSelect = (section: SECTION) => {
+const handleSelect = (section, url) => {
   sectionStore.selectSection(section);
+  router.push(url);
 };
-const isMobileResponsive = computed(() => width.value < 768);
 const isReelsSelect = computed(
   () => sectionStore.getSectionSelect === SECTION.REELS && isMobileResponsive
 );
@@ -26,19 +28,19 @@ const isReelsSelect = computed(
     } md:hidden`"
   >
     <div class="mt-3 flex items-center justify-around">
-      <div @click="handleSelect(SECTION.HOME)">
+      <div @click="handleSelect(SECTION.HOME, '/')">
         <HomeIcon :isSelect="section === SECTION.HOME" />
       </div>
-      <div @click="handleSelect(SECTION.SEARCH)">
+      <div @click="handleSelect(SECTION.SEARCH, '/explore/')">
         <Search :isSelect="section === SECTION.SEARCH" />
       </div>
-      <div @click="handleSelect(SECTION.REELS)">
+      <div @click="handleSelect(SECTION.REELS, '/reels/')">
         <Reels :isSelect="section === SECTION.REELS" />
       </div>
-      <div @click="handleSelect(SECTION.ACTIVITYFEED)">
+      <div @click="handleSelect(SECTION.ACTIVITYFEED, '/activity-feed')">
         <ActivityFeed :isSelect="section === SECTION.ACTIVITYFEED" />
       </div>
-      <div @click="handleSelect(SECTION.SELF)">
+      <div @click="handleSelect(SECTION.SELF, '/cuzknothz/')">
         <SelfAvatar :isSelect="section === SECTION.SELF" />
       </div>
     </div>
