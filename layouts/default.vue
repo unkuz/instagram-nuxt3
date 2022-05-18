01c732c5e7b9d17583cd8865aca93e6a58017b3f
cuzknothz <cuzknothz@gmail.com>
2022-05-19 00:12:33 +0700

    +

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
 import Navbarbottom from "./navbarbottom.vue";
 import Header from "~~/components/Header/index.vue";
+const router = useRouter();
+const { width } = useResizeWindow();
+const isMobileResponsive = computed(() => width.value < 768);
 </script>
 
 <template>
   <div>
-    <Header />
+    <Header
+      v-if="
+        !(router.currentRoute.value.path = '/explore/' && isMobileResponsive)
+      "
+    />
     <div class="mx-auto mt-[50px] w-full sm:mt-[84px] lg:w-[935px]">
       <slot />
     </div>
