d44a39bef95e29e4a5d3d45f56e636b8c6969001
cuzknothz <cuzknothz@gmail.com>
2022-05-19 23:39:47 +0700

    _

--- components/Header/Logo.vue
+++ components/Header/Logo.vue
@@ -1,13 +1,21 @@
 <script lang="ts" setup>
 import { ROUTES } from '~~/constants/routes'
+import { SECTION } from '~~/constants/section'
+import { useGlobalStore } from '~~/store/global'
+
+const globalStore = useGlobalStore()
+const router = useRouter()
+const handleSelect = (section: SECTION, url?: string) => {
+  useDebounceRoute(url, router)
+  globalStore.setSection(section)
+}
 </script>
 
 <template>
-  <NuxtLink :to="ROUTES.HOME">
-    <img
-      alt="Instagram"
-      class="mt-[7px] h-[29px] cursor-pointer"
-      src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
-    />
-  </NuxtLink>
+  <img
+    alt="Instagram"
+    @click="handleSelect(SECTION.HOME, '/')"
+    class="mt-[7px] h-[29px] cursor-pointer select-none"
+    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+  />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["~/assets/css/index.css"],
+  css: ['~/assets/css/index.css'],
   build: {
     postcss: {
       postcssOptions: {
@@ -13,17 +12,5 @@ export default defineNuxtConfig({
       },
     },
   },
-  buildModules: ["@pinia/nuxt", "@nuxtjs/pwa"],
-  pwa: {
-    icon: false, // disables the icon module
-    meta: {
-      title: "My PWA",
-      author: "Me",
-    },
-    manifest: {
-      name: "Nuxt.js PWAs are so easy",
-      short_name: "Nuxt.js PWA",
-      lang: "en",
-    },
-  },
-});
+  buildModules: ['@pinia/nuxt'],
+})
