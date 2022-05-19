087c284b38b2dee84d9e76f9aaae419854e5c0b9
cuzknothz <cuzknothz@gmail.com>
2022-05-20 00:26:41 +0700

    _

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useResizeWindow } from '~~/composables/useResizeWindow'
 import { useGlobalStore } from '~~/store/global'
+import Transition from '~~/components/Transition/index.vue'
 
 const globalStore = useGlobalStore()
 const isTransition = computed(() => globalStore.getIsTransition)
@@ -17,7 +18,42 @@ watch([width, height], () => {
     </NuxtLayout>
     <div
       v-if="isTransition"
-      class="absolute inset-0 z-50 flex animate-pulse items-center justify-center bg-fuchsia-400 opacity-30"
+      class="absolute inset-0 z-40 flex items-center justify-center bg-sky-300 transition"
     ></div>
   </div>
 </template>
+<style scoped>
+.transition {
+  animation: transition 1s linear;
+}
+
+@keyframes transition {
+  0% {
+    clip-path: polygon(100% 0, 100% 0, 100% 100%, 100% 100%);
+  }
+  10% {
+    clip-path: polygon(82% 0, 100% 0, 100% 100%, 68% 100%);
+  }
+  20% {
+    clip-path: polygon(65% 0, 100% 0, 100% 100%, 44% 100%);
+  }
+  30% {
+    clip-path: polygon(24% 0, 100% 0, 100% 100%, 10% 100%);
+  }
+  40% {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+  }
+  50% {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+  }
+  60% {
+    clip-path: polygon(0 0, 95% 0, 75% 100%, 0 100%);
+  }
+  80% {
+    clip-path: polygon(0 0, 59% 0, 30% 100%, 0 100%);
+  }
+  100% {
+    clip-path: polygon(0 0, 0 0, 0 100%, 0 100%);
+  }
+}
+</style>
