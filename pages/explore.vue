c1ffda67bfc877fce5e4ea4630b77e0d96409207
cuzknothz <cuzknothz@gmail.com>
2022-05-21 12:42:56 +0700

    _

--- pages/explore.vue
+++ pages/explore.vue
@@ -1,94 +1,92 @@
 <script lang="ts" setup>
 definePageMeta({
-  layout: 'main-not-header',
+  layout: 'main',
 })
 </script>
 
 <template>
   <div class="grid h-[500px] w-full grid-cols-3 gap-[2px] md:gap-[28px]">
-    <div class="aspect-square overflow-hidden bg-fuchsia-300">
+    <div class="aspect-square overflow-hidden">
       <img
         class="aspect-square w-full object-cover"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
 
-    <div class="aspect-square bg-fuchsia-300 md:col-start-1 md:row-start-2">
+    <div class="aspect-square md:col-start-1 md:row-start-2">
       <img
         class="aspect-square w-full object-cover"
         src="/explore/278009228_960257008015789_1126924171986179644_n.jpg"
       />
     </div>
-    <div
-      class="col-start-3 row-span-2 row-start-1 bg-fuchsia-300 object-cover md:col-span-2 md:col-start-2"
-    >
+    <div class="col-start-3 row-span-2 row-start-1 object-cover md:col-span-2 md:col-start-2">
       <img
         class="aspect-auto h-full w-full object-cover md:aspect-square"
         src="/explore/275624700_382008663748898_3572544032225631298_n.jpg"
       />
     </div>
 
-    <div class="aspect-square bg-fuchsia-300">
+    <div class="aspect-square">
       <img
         class="aspect-square w-full object-cover"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
-    <div class="aspect-square bg-fuchsia-300">
+    <div class="aspect-square">
       <img
         class="aspect-square w-full object-cover"
         src="/explore/275624700_382008663748898_3572544032225631298_n.jpg"
       />
     </div>
-    <div class="aspect-square bg-fuchsia-300">
+    <div class="aspect-square">
       <img
         class="aspect-auto w-full object-cover md:aspect-square"
         src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg"
       />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/269745549_1653199611678007_2927572044521418605_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/269745549_1653199611678007_2927572044521418605_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/278009228_960257008015789_1126924171986179644_n.jpg" />
     </div>
     <!-- line -->
-    <div class="aspect-square w-full bg-fuchsia-300 md:col-span-2 md:row-span-2">
+    <div class="aspect-square w-full md:col-span-2 md:row-span-2">
       <img src="/explore/269745549_1653199611678007_2927572044521418605_n.jpg" />
     </div>
 
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/269745549_1653199611678007_2927572044521418605_n.jpg" />
     </div>
-    <div class="row-span-2 w-full bg-fuchsia-300 md:row-span-1 md:aspect-square">
+    <div class="row-span-2 w-full md:row-span-1 md:aspect-square">
       <img
         class="h-full object-cover md:h-auto"
         src="/explore/269745549_1653199611678007_2927572044521418605_n.jpg"
       />
     </div>
 
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/278009228_960257008015789_1126924171986179644_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/275624700_382008663748898_3572544032225631298_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/280053464_1025075078373353_2175908518960908191_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/275624700_382008663748898_3572544032225631298_n.jpg" />
     </div>
-    <div class="aspect-square w-full bg-fuchsia-300">
+    <div class="aspect-square w-full">
       <img src="/explore/275624700_382008663748898_3572544032225631298_n.jpg" />
     </div>
   </div>
