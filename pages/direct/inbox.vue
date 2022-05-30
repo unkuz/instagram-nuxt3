b0b14bd0dddba81f7e060d68f15701b342d4eddc
cuzknothz <cuzknothz@gmail.com>
2022-05-30 23:30:54 +0700

    _

--- pages/direct/inbox.vue
+++ pages/direct/inbox.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useGlobalStore } from '~~/store/global'
 import Navbarbottom from '~~/layouts/navbarbottom.vue'
+import Search from '~~/components/Header/Search.vue'
 const globalStore = useGlobalStore()
 const isMobile = computed(() => globalStore.getIsMobile)
 </script>
@@ -9,15 +10,17 @@ const isMobile = computed(() => globalStore.getIsMobile)
   <div>
     <Header v-if="!isMobile" />
     <div class="mx-auto mt-[50px] w-full sm:mt-[84px] lg:w-[935px]">
-      <div class="flex h-screen w-full items-stretch border-[1px] border-gray-300">
+      <div class="flex h-screen w-full items-stretch border-0 border-gray-300 md:border-[1px]">
         <div class="w-full border-r-[1px] border-gray-300 md:min-w-[350px]">
           <div
+            v-if="!isMobile"
             class="flex h-[60px] items-center justify-between border-b-[1px] border-gray-300 px-[20px]"
           >
             <div class="h-0 w-[32px]"></div>
             <div>CUZKNOTHZ</div>
             <div>icon</div>
           </div>
+          <div v-else class="flex h-[60px] items-center justify-center bg-red-300"><Search /></div>
           <div class="text-sm">
             <div
               class="flex h-[72px] w-full cursor-pointer items-center px-[20px] py-[8px] hover:bg-gray-200"
@@ -75,7 +78,7 @@ const isMobile = computed(() => globalStore.getIsMobile)
             </div>
           </div>
         </div>
-        <div class="block w-full md:hidden"></div>
+        <div v-if="!isMobile" class="w-full"></div>
       </div>
     </div>
 
