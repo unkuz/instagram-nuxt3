1034bef452c61f2a8795e6f157ce0eaf6a9aa0a1
cuzknothz <cuzknothz@gmail.com>
2022-05-31 16:28:25 +0700

    _

--- pages/css.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <div class="flex h-[500px] w-full bg-green-200">
-    <h3 class="mx-auto leading-[500px]">hahah</h3>
-  </div>
-</template>
--- pages/lock.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-// useLockScroll();
-</script>
-
-<template>
-  <div class="h-[6000px] bg-red-300"></div>
-</template>
