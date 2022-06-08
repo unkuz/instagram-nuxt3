e0032497388ceae513880235507e0469d1b896e3
cuzknothz <cuzknothz@gmail.com>
2022-06-08 23:35:58 +0700

    _

--- layouts/main.vue
+++ layouts/main.vue
@@ -1,17 +1,38 @@
 <script lang="ts" setup>
 import Navbarbottom from './navbarbottom.vue'
 import Header from '~~/components/Header/index.vue'
+import { useGlobalStore } from '~~/store/global'
+import { SECTION } from '~~/constants/section'
+import { useDebounceRoute } from '~~/composables/useDeboundRoute'
+
+const globalStore = useGlobalStore()
+const section = computed(() => globalStore.getSection)
+const isMobile = computed(() => globalStore.getIsMobile)
 const router = useRouter()
-const { width } = useResizeWindow()
-const isMobileResponsive = computed(() => width.value < 768)
+const handleSelect = (section: SECTION, url: string = '') => {
+  useDebounceRoute(url, router)
+  globalStore.setSection(section)
+}
 </script>
 
 <template>
   <div>
-    <Header />
+    <div
+      v-if="isMobile && section === SECTION.MESSENGER"
+      class="flex h-[60px] items-center justify-between border-[1px] border-gray-300 bg-white px-[20px]"
+    >
+      <svg
+        class="h-[25px] w-[25px] rotate-180"
+        viewBox="0 0 24 24"
+        @click="handleSelect(SECTION.HOME, '/')"
+      >
+        <path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z" />
+      </svg>
+    </div>
+    <Header v-else />
     <div class="mx-auto mt-[50px] w-full sm:mt-[84px] lg:w-[935px]">
       <slot />
     </div>
-    <Navbarbottom />
+    <Navbarbottom v-if="!(isMobile && section === SECTION.MESSENGER)" />
   </div>
 </template>
