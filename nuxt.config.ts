1c0677831b33d547d6fe41e88b5b4c3397c404f5
cuzknothz <cuzknothz@gmail.com>
2022-06-12 22:47:07 +0700

    _

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,13 +12,5 @@ export default defineNuxtConfig({
       },
     },
   },
-  buildModules: ['@pinia/nuxt', '@nuxtjs/pwa'],
-  pwa: {
-    icon: false, // disables the icon module
-    manifest: {
-      name: 'My Awesome App',
-      lang: 'fa',
-      useWebmanifestExtension: false,
-    },
-  },
+  buildModules: ['@pinia/nuxt'],
 })
