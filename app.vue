3003da78472b3e90aa6302578f86e1ef3eac2eab
cuzknothz <cuzknothz@gmail.com>
2022-06-24 21:42:42 +0700

    _

--- app.vue
+++ app.vue
@@ -14,6 +14,13 @@ import { usePrelude } from '~~/composables/usePrelude'
 
 useHead({
   title: 'Instagram',
+  meta: [
+    {
+      name: 'viewport',
+      content:
+        'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no',
+    },
+  ],
 })
 
 const globalStore = useGlobalStore()
