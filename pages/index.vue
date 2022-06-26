2fd974bca2ef1620e0050d036ed39d5c1c69388a
cuzknothz <cuzknothz@gmail.com>
2022-06-27 00:00:28 +0700

    dfdf

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,22 @@ import HeroPagelet from '~~/components/HeroPagelet/index.vue'
 import Suggestions from '~~/components/Huge/Suggestions/index.vue'
 import Post from '~~/components/Post/index.vue'
 import { useWindowResizeCallback } from '~~/composables/useWindowResizeCallback'
-// import NewPost from '~~/components/Huge/NewPost/index.vue
-import { useGlobalStore } from '~~/store/global'
-import { SECTION } from '~~/constants/section'
-import BackDrop from '~~/components/Utils/BackDrop.vue'
 const mainRef = ref(null)
 const leftRef = ref(null)
 definePageMeta({
   layout: 'main',
 })
 
-// const rightRef = ref(null)
+const rightRef = ref(null)
 
-// onMounted(() => {
-//   rightRef.value.style.left = `${leftRef.value.offsetLeft + 642}px`
-//   console.log('KAKA', leftRef.value.offsetLeft)
-// })
+onMounted(() => {
+  rightRef.value.style.left = `${leftRef.value.offsetLeft + 642}px`
+  console.log('KAKA', leftRef.value.offsetLeft)
+})
 
-// useWindowResizeCallback(() => {
-//   rightRef.value.style.left = `${leftRef.value.offsetLeft + 642}px`
-// })
+useWindowResizeCallback(() => {
+  rightRef.value.style.left = `${leftRef.value.offsetLeft + 642}px`
+})
 </script>
 
 <template>
@@ -34,12 +30,12 @@ definePageMeta({
           <Post />
         </div>
       </div>
-      <!-- <div
+      <div
         class="fixed top-[84px] left-[20000px] hidden h-[473px] w-[293px] bg-white text-sm lg:block"
         ref="rightRef"
       >
         <Suggestions />
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
