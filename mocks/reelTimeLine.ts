f6b2700d6f2eaa0eeebdda9aa2458833a8daf530
cuzknothz <cuzknothz@gmail.com>
2022-07-16 02:24:49 +0700

    asdf

--- mocks/reelTimeLine.ts
+++ mocks/reelTimeLine.ts
@@ -1,6 +1,68 @@
 export const timeLine = [
   {
     created_at: 1657809031,
+    id: 'CaM4QBLJalo',
+    location: {
+      short_name: 'Copenhagen',
+    },
+    comments: [
+      {
+        text: "Why does that necklace look like a soul reaper pendant I fuckin love it where'd you buy it",
+        created_at: 1637435588,
+        user: {
+          pk: 20433830474,
+          username: 'samael_morningstar1220',
+          full_name: 'samael_morningstar1220',
+          is_private: false,
+          profile_pic_url: '/timeline/274034661_117505720749197_8602961543437501906_n.jpg',
+        },
+        comment_like_count: 1,
+        reply: [
+          {
+            text: 'Shut up I wanna marry u',
+            created_at: 1637435588,
+            user: {
+              pk: 20433830474,
+              username: 'miaxmewgulfness',
+              full_name: 'miaxmewgulfness',
+              is_private: false,
+              profile_pic_url: '/timline/292244620_148187957799346_530540740971734479_n.jpg',
+            },
+            comment_like_count: 3,
+          },
+        ],
+      },
+    ],
+    carousel_media: {
+      images: [
+        {
+          id: 1,
+          src: '/timeline/274515413_1131169894307572_433591226677794101_n.jpg',
+        },
+        {
+          id: 2,
+          src: '/timeline/274205784_1189639965176591_5834702774638907502_n.jpg',
+        },
+      ],
+      videos: [],
+    },
+    user: {
+      id: 'kristenstewart_0519',
+      username: 'kristenstewart_0519',
+      full_name: 'kristenstewart_0519',
+      profile_pic_url: '/timeline/277644162_1065111897422838_997966133366016545_n.jpg',
+      friendship_status: {
+        following: true,
+        outgoing_request: false,
+      },
+    },
+    like_count: 3692,
+    has_liked: false,
+    caption_text: 'Sooo cute 🥰🤍🤍',
+    is_seen: true,
+  },
+  {
+    created_at: 1657809031,
     id: 'CWgO_gAIJxB',
     location: {
       short_name: 'Copenhagen',
@@ -115,68 +177,6 @@ export const timeLine = [
     has_liked: false,
     caption_text:
       "Mini katana ☆\n-\nNew Hair cut ❤️✂️\nNow there's hair all over my sink again xD\n-\nMy dream hair cut is called the Jelly Fish 🐚",
-    is_seen: true,
-  },
-  {
-    created_at: 1657809031,
-    id: 'CaM4QBLJalo',
-    location: {
-      short_name: 'Copenhagen',
-    },
-    comments: [
-      {
-        text: "Why does that necklace look like a soul reaper pendant I fuckin love it where'd you buy it",
-        created_at: 1637435588,
-        user: {
-          pk: 20433830474,
-          username: 'samael_morningstar1220',
-          full_name: 'samael_morningstar1220',
-          is_private: false,
-          profile_pic_url: '/timeline/274034661_117505720749197_8602961543437501906_n.jpg',
-        },
-        comment_like_count: 1,
-        reply: [
-          {
-            text: 'Shut up I wanna marry u',
-            created_at: 1637435588,
-            user: {
-              pk: 20433830474,
-              username: 'miaxmewgulfness',
-              full_name: 'miaxmewgulfness',
-              is_private: false,
-              profile_pic_url: '/timline/292244620_148187957799346_530540740971734479_n.jpg',
-            },
-            comment_like_count: 3,
-          },
-        ],
-      },
-    ],
-    carousel_media: {
-      images: [
-        {
-          id: 1,
-          src: '/timeline/274515413_1131169894307572_433591226677794101_n.jpg',
-        },
-        {
-          id: 2,
-          src: '/timeline/274205784_1189639965176591_5834702774638907502_n.jpg',
-        },
-      ],
-      videos: [],
-    },
-    user: {
-      id: 'kristenstewart_0519',
-      username: 'kristenstewart_0519',
-      full_name: 'kristenstewart_0519',
-      profile_pic_url: '/timeline/277644162_1065111897422838_997966133366016545_n.jpg',
-      friendship_status: {
-        following: true,
-        outgoing_request: false,
-      },
-    },
-    like_count: 3692,
-    has_liked: false,
-    caption_text: 'Sooo cute 🥰🤍🤍',
     is_seen: true,
   },
 ]
