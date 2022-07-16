a127aa0e4782ecc51b87da93951ce37f5d8db978
cuzknothz <cuzknothz@gmail.com>
2022-07-16 11:12:06 +0700

    _

--- pages/_____/p/[id].vue
+++ pages/_____/p/[id].vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
+import HeartIcon_ from '~~/assets/svg/heart_icon.svg'
+import MoreIcon_ from '~~/assets/svg/more_icon.svg'
 import BackDrop from '~~/components/Utils/BackDrop.vue'
-import { useWindowResizeCallback } from '~~/composables/useWindowResizeCallback'
-import { APP_ROUTES } from '~~/routes'
-import { useViewPostStore } from '~~/store/viewPost'
 import { useLockScroll } from '~~/composables/useLockScroll'
+import { useWindowResizeCallback } from '~~/composables/useWindowResizeCallback'
 import { useMoreStore } from '~~/store/more'
 
 const viewPostRef = ref(null)
@@ -50,20 +50,7 @@ useWindowResizeCallback(calcHeightComment)
                 <p class="cursor-pointer">cuzknothz</p>
               </div>
               <div>
-                <svg
-                  aria-label="More Options"
-                  class="cursor-pointer"
-                  color="#262626"
-                  fill="#262626"
-                  height="24"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  width="24"
-                >
-                  <circle cx="12" cy="12" r="1.5"></circle>
-                  <circle cx="6" cy="12" r="1.5"></circle>
-                  <circle cx="18" cy="12" r="1.5"></circle>
-                </svg>
+                <MoreIcon_ />
               </div>
             </div>
           </div>
@@ -92,19 +79,7 @@ useWindowResizeCallback(calcHeightComment)
                 </div>
               </div>
               <div class="cursor-pointer">
-                <svg
-                  aria-label="Like"
-                  color="#262626"
-                  fill="#262626"
-                  height="12"
-                  role="img"
-                  viewBox="0 0 24 24"
-                  width="12"
-                >
-                  <path
-                    d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"
-                  ></path>
-                </svg>
+                <HeartIcon_ />
               </div>
             </div>
           </div>
