a36b0d1d0da9dd274fb5f86772194deeed5b23d3
cuzknothz <cuzknothz@gmail.com>
2022-07-17 00:56:47 +0700

    _

--- components/Utils/Prelude.vue
+++ components/Utils/Prelude.vue
@@ -2,6 +2,14 @@
 import { useLockScroll } from '~~/composables/useLockScroll'
 import Logo from '~~/assets/svg/logo.svg'
 
+const isReadyFont = ref(false)
+
+onBeforeMount(() => {
+  document.fonts.ready.then(() => {
+    isReadyFont.value = true
+  })
+})
+
 useLockScroll()
 </script>
 
@@ -10,6 +18,11 @@ useLockScroll()
     class="fixed top-0 left-0 z-20 flex h-screen w-full flex-col items-center justify-center space-y-[50px] bg-white"
   >
     <div class="w-[100px] lg:w-[120px]"><Logo /></div>
-    <p class="font-august text-[1.7rem] uppercase tracking-wide lg:text-[1.8rem]">cuzknothz</p>
+    <p
+      v-if="isReadyFont"
+      class="font-august text-[1.7rem] uppercase tracking-wide lg:text-[1.8rem]"
+    >
+      cuzknothz
+    </p>
   </div>
 </template>
