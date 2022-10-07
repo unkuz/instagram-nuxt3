b5ad9ef0ffaad2606b50544061920531649b88c5
cuzknothz <cuzknothz@gmail.com>
2022-10-07 13:13:23 +0700

    _

--- components/Utils/Emoji.vue
+++ components/Utils/Emoji.vue
@@ -7,11 +7,12 @@ import { emoji } from '~~/constants/emoji'
     <div
       class="absolute -top-[310px] h-[300px] overflow-y-scroll border-[1px] border-gray-300 bg-white"
     >
-      <div v-for="topic in emoji">
+      <div v-for="(topic, idx) in emoji" :key="idx">
         <p class="mt-[10px] h-[20px] pl-[10px] text-[0.8rem] font-[500]">{{ topic.label }}</p>
         <div class="grid w-[280px] grid-cols-7 text-[1.5rem]">
           <div
-            v-for="i in topic.icons"
+            v-for="(i, idx) in topic.icons"
+            :key="idx"
             @click="$emit('emoji-add', i)"
             class="flex h-[40px] cursor-pointer select-none items-center justify-center"
           >
