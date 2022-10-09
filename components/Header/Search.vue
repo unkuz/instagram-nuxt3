b624edc9f9471add863393928f7b2eaf3efa4a30
cuzknothz <cuzknothz@gmail.com>
2022-10-09 09:42:27 +0700

    _

--- components/Header/Search.vue
+++ components/Header/Search.vue
@@ -11,7 +11,9 @@ const isSearchActive = computed(() => searchStore.isFocus)
 const isSearchHaveValue = computed(() => searchStore.getIsSearchHaveValue)
 
 watch(isSearchActive, (state) => {
-  state ? inputSearch.value.focus() : ''
+  if (state) {
+    inputSearch.value.focus()
+  }
 })
 
 watch([isSearchHaveValue, isSearchActive], () => {
@@ -28,17 +30,7 @@ useClickOutSide(searchRef, () => {
   searchStore.setIsFocus(false)
 })
 
-const setActiveSearchInput = () => {
-  searchStore.setIsFocus(true)
-}
-
-const closeSearch = (e: MouseEvent) => {
-  e.stopPropagation()
-  searchStore.clearValue()
-  searchStore.setIsFocus(false)
-}
-
-const handleInputValueSearch = (e: Event) => {
+const handleInputSearch = (e: Event) => {
   searchStore.setValue((e.target as HTMLInputElement).value)
 }
 </script>
@@ -46,29 +38,21 @@ const handleInputValueSearch = (e: Event) => {
 <template>
   <div
     ref="searchRef"
-    @click="setActiveSearchInput"
+    @click="searchStore.setIsFocus(true)"
     class="relative flex h-[36px] w-[268px] cursor-text items-center rounded-md bg-gray-200 px-[16px]"
   >
     <SearchIcon v-show="!isSearchActive" />
-    <span v-show="!isSearchHaveValue" class="absolute top-1/2 left-[60px] -translate-y-1/2 text-sm"
+    <span
+      v-show="!isSearchActive && !isSearchHaveValue"
+      class="absolute top-1/2 left-[60px] -translate-y-1/2 text-sm"
       >Search</span
     >
     <input
       type="text"
       ref="inputSearch"
       :value="searchValue"
-      @input="handleInputValueSearch"
+      @input="handleInputSearch"
       class="absolute top-1/2 left-1/2 h-[30px] w-[236px] -translate-x-1/2 -translate-y-1/2 bg-transparent text-sm caret-[#09f] focus:outline-none"
     />
-    <div class="absolute right-3 cursor-pointer" v-show="isSearchActive" @click="closeSearch">
-      <div class="relative h-[18px] w-[18px] rounded-full bg-gray-600">
-        <div
-          class="absolute top-1/2 left-1/2 h-[8px] w-[2px] -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white"
-        ></div>
-        <div
-          class="absolute top-1/2 left-1/2 h-[8px] w-[2px] -translate-x-1/2 -translate-y-1/2 -rotate-45 bg-white"
-        ></div>
-      </div>
-    </div>
   </div>
 </template>
