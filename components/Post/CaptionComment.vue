a4cfcf97053e1f775923f93cc5d839c797667878
cuzknothz <cuzknothz@gmail.com>
2022-10-09 08:58:33 +0700

    _

--- components/Post/CaptionComment.vue
+++ components/Post/CaptionComment.vue
@@ -18,31 +18,31 @@ defineProps<IProps>()
     <div class="mb-[5px] h-[18px] cursor-pointer font-medium text-red-400">
       {{ like_count }} likes
     </div>
-    <div class="mb-[4px]">
-      <p class="overflow-hidden text-ellipsis">
-        <div class="font-medium">
-          <div class="inline-block bg-gray-500 px-[10px] py-[1px] font-[500] text-white">{{
-            user.username
-          }}</div></div
-        >
-        <div class="inline-block bg-gray-100 "> {{ caption_text }}</div>
-      </p>
+    <div class="inline-block bg-gray-500 px-[10px] py-[1px] font-[500] text-white">
+      {{ user.username }}
     </div>
-    <div class="mt-[5px] flex gap-[8px]">
-      <Button
-        text="#nuxt3"
-        className="text-[0.8rem] !px-[7px]    !bg-fuchsia-400 shadow-md shadow-fuchsia-400/40"
-      />
-      <Button text="#vue3" className="text-[0.8rem] !px-[7px]   !bg-gray-300 shadow-gray-300/40" />
-      <Button
-        text="#django"
-        className="text-[0.8rem] !px-[7px]   !bg-yellow-200 shadow-yellow-200/40"
-      />
-      <Button
-        text="#reactJS"
-        className="text-[0.8rem] !px-[7px]   !bg-blue-300 shadow-blue-300/40"
-      />
+    <div class="mt-[5px] bg-gray-100/60 py-[5px]">
+      <div>{{ caption_text }}</div>
+      <div class="mt-[5px] flex gap-[8px]">
+        <Button
+          text="#nuxt3"
+          className="text-[0.8rem] !px-[7px]    !bg-fuchsia-400 shadow-md shadow-fuchsia-400/40"
+        />
+        <Button
+          text="#vue3"
+          className="text-[0.8rem] !px-[7px]   !bg-gray-300 shadow-gray-300/40"
+        />
+        <Button
+          text="#django"
+          className="text-[0.8rem] !px-[7px]   !bg-yellow-200 shadow-yellow-200/40"
+        />
+        <Button
+          text="#reactJS"
+          className="text-[0.8rem] !px-[7px]   !bg-blue-300 shadow-blue-300/40"
+        />
+      </div>
     </div>
+
     <div class="m-[8px_0px_5px_0px] h-[18px] text-[0.8rem] text-gray-400">
       {{ moment('Thu Oct 1 2022 17:30:03 GMT+0300').fromNow() }}
     </div>
