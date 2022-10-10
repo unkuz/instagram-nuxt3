5d4a2e1f67cca4e49ffd80906a320664fb6eae6f
cuzknothz <cuzknothz@gmail.com>
2022-10-10 11:56:02 +0700

    _

--- store/todo.ts
+++ store/todo.ts
@@ -1,67 +1,67 @@
-import { defineStore } from 'pinia'
-import { v4 } from 'uuid'
+// import { defineStore } from 'pinia'
+// import { v4 } from 'uuid'
 
-export interface Todo {
-  id: string
-  title: string
-  done: Boolean
-  createAt: Date
-  updateAt: Date
-}
-export interface TodoState {
-  items: Todo[] | undefined[]
-  haha: Boolean
-}
+// export interface Todo {
+//   id: string
+//   title: string
+//   done: Boolean
+//   createAt: Date
+//   updateAt: Date
+// }
+// export interface TodoState {
+//   items: Todo[] | undefined[]
+//   haha: Boolean
+// }
 
-export interface UpdateTodo {
-  title?: string
-  done?: Boolean
-}
+// export interface UpdateTodo {
+//   title?: string
+//   done?: Boolean
+// }
 
-export interface TodoAdd {
-  title: string
-}
+// export interface TodoAdd {
+//   title: string
+// }
 
-const state = (): TodoState => ({
-  items: [],
-  haha: true,
-})
+// const state = (): TodoState => ({
+//   items: [],
+//   haha: true,
+// })
 
-const getters = {
-  getById: (state: TodoState) => (id: string) => {
-    return state.items.find((i: Todo) => i.id === id)
-  },
-  getOrderedTodo: (state: TodoState) => {
-    return state.items.sort((a: Todo, b: Todo) => a.createAt.getTime() - b.createAt.getTime())
-  },
-}
+// const getters = {
+//   getById: (state: TodoState) => (id: string) => {
+//     return state.items.find((i: Todo) => i.id === id)
+//   },
+//   getOrderedTodo: (state: TodoState) => {
+//     return state.items.sort((a: Todo, b: Todo) => a.createAt.getTime() - b.createAt.getTime())
+//   },
+// }
 
-const actions = {
-  add(partialTodo: TodoAdd) {
-    const todo: Todo = {
-      id: v4(),
-      done: false,
-      createAt: new Date(),
-      updateAt: new Date(),
-      ...partialTodo,
-    }
-    this.items.push(todo)
-  },
-  remove(id: string) {
-    this.items = this.items.filter((i: Todo) => i.id !== id)
-  },
-  update(id: string, data: UpdateTodo) {
-    this.items = this.items.map((i: Todo) =>
-      i.id === id ? { ...i, ...data, updateAt: new Date() } : i
-    )
-  },
-  changeHaha() {
-    this.haha = false
-  },
-}
+// const actions = {
+//   add(partialTodo: TodoAdd) {
+//     const todo: Todo = {
+//       id: v4(),
+//       done: false,
+//       createAt: new Date(),
+//       updateAt: new Date(),
+//       ...partialTodo,
+//     }
+//     this.items.push(todo)
+//   },
+//   remove(id: string) {
+//     this.items = this.items.filter((i: Todo) => i.id !== id)
+//   },
+//   update(id: string, data: UpdateTodo) {
+//     this.items = this.items.map((i: Todo) =>
+//       i.id === id ? { ...i, ...data, updateAt: new Date() } : i
+//     )
+//   },
+//   changeHaha() {
+//     this.haha = false
+//   },
+// }
 
-export const useTodoStore = defineStore('todoStore', {
-  state,
-  getters,
-  actions,
-})
+// export const useTodoStore = defineStore('todoStore', {
+//   state,
+//   getters,
+//   actions,
+// })
