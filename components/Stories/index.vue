6c4a41e8a63e76f89f7090a4b2f379816141c6cf
cuzknothz <cuzknothz@gmail.com>
2022-10-11 20:56:58 +0700

    _

--- components/Stories/index.vue
+++ components/Stories/index.vue
@@ -1,53 +1,26 @@
-<script lang="ts" setup>
-import ArrowIcon_ from '@@/assets/svg/arrow_icon.svg'
-import { SIZE_HERO_PAGELET } from '@@/constants'
+<script lang="ts">
 import { IStory } from '@@/models'
+import { useKeenSlider } from 'keen-slider/vue.es'
 import Story from './Story.vue'
+import 'keen-slider/keen-slider.min.css'
 
-interface IProps {
-  stories: IStory[]
+export default {
+  setup() {
+    const [container] = useKeenSlider({
+      slides: {
+        perView: 7,
+        spacing: 0,
+      },
+    })
+    return { container }
+  },
+  props: {
+    stories: Array<IStory>,
+  },
+  components: {
+    Story,
+  },
 }
-
-defineProps<IProps>()
-
-const heroPageletRef = ref<HTMLElement>(null)
-const startPointX = ref(0)
-const isShowPrev = ref(false)
-const isShowNext = ref(true)
-
-onMounted(() => {
-  if (heroPageletRef && heroPageletRef.value.clientWidth < SIZE_HERO_PAGELET.WIDTH) {
-    isShowNext.value = false
-  }
-})
-
-const handle = (e: MouseEvent, idx: number) => {
-  heroPageletRef.value.style.transform = `translateX(${
-    startPointX.value - SIZE_HERO_PAGELET.SPACE_SLICE * idx
-  }px)`
-  startPointX.value -= SIZE_HERO_PAGELET.SPACE_SLICE * idx
-  if (
-    idx === 1 &&
-    Math.abs(startPointX.value - SIZE_HERO_PAGELET.WIDTH) > heroPageletRef.value.clientWidth
-  ) {
-    isShowNext.value = false
-    heroPageletRef.value.style.transform = `translateX(-${
-      heroPageletRef.value.clientWidth - SIZE_HERO_PAGELET.WIDTH
-    }px)`
-    startPointX.value = -(heroPageletRef.value.clientWidth - SIZE_HERO_PAGELET.WIDTH)
-  } else {
-    isShowNext.value = true
-  }
-  if (idx === -1 && startPointX.value >= 0) {
-    heroPageletRef.value.style.transform = `translateX(0px)`
-    startPointX.value = 0
-    isShowPrev.value = false
-  } else {
-    isShowPrev.value = true
-  }
-}
-
-const handleTouchStart = (e: TouchEvent) => {}
 </script>
 
 <template>
@@ -55,17 +28,18 @@ const handleTouchStart = (e: TouchEvent) => {}
     class="relative inline-flex h-[119px] w-full items-center overflow-hidden border-0 border-gray-200 shadow-gray-200 sm:mb-[24px] sm:border-[1px] sm:shadow-sm"
   >
     <div
-      class="flex h-[85px] transition-all duration-500 ease-in-out"
-      ref="heroPageletRef"
-      @touchstart="handleTouchStart"
+      ref="container"
+      class="keen-slider h-[85px] w-full transition-all duration-500 ease-in-out"
     >
-      <Story v-for="i in stories" v-bind="i" :key="i.id" />
+      <div class="keen-slider__slide" v-for="i in stories">
+        <Story v-bind="i" :key="i.id" />
+      </div>
     </div>
     <div class="absolute top-1/2 right-2 -translate-y-1/2">
-      <ArrowIcon_ v-show="isShowNext" @click="handle($event, 1)" />
+      <ArrowIcon_ />
     </div>
     <div class="absolute top-1/2 left-2 -translate-y-1/2">
-      <ArrowIcon_ class="rotate-180" v-show="isShowPrev" @click="handle($event, -1)" />
+      <ArrowIcon_ class="rotate-180" />
     </div>
   </div>
 </template>
---
+++ pages/keen.vue
@@ -0,0 +1,78 @@
+<template>
+  <div ref="container" class="keen-slider">
+    <div class="keen-slider__slide number-slide1">1</div>
+    <div class="keen-slider__slide number-slide2">2</div>
+    <div class="keen-slider__slide number-slide3">3</div>
+    <div class="keen-slider__slide number-slide4">4</div>
+    <div class="keen-slider__slide number-slide5">5</div>
+    <div class="keen-slider__slide number-slide6">6</div>
+  </div>
+</template>
+
+<script>
+import { useKeenSlider } from 'keen-slider/vue.es'
+import 'keen-slider/keen-slider.min.css'
+
+export default {
+  setup() {
+    const [container] = useKeenSlider({
+      slides: {
+        perView: 3,
+        spacing: 15,
+      },
+    })
+    return { container }
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+[class^='number-slide'],
+[class*=' number-slide'] {
+  background: grey;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 50px;
+  color: #fff;
+  font-weight: 500;
+  height: 300px;
+  max-height: 100vh;
+}
+
+.number-slide1 {
+  background: rgb(64, 175, 255);
+  background: linear-gradient(128deg, rgba(64, 175, 255, 1) 0%, rgba(63, 97, 255, 1) 100%);
+}
+
+.number-slide2 {
+  background: rgb(255, 75, 64);
+  background: linear-gradient(128deg, rgba(255, 154, 63, 1) 0%, rgba(255, 75, 64, 1) 100%);
+}
+
+.number-slide3 {
+  background: rgb(182, 255, 64);
+  background: linear-gradient(128deg, rgba(182, 255, 64, 1) 0%, rgba(63, 255, 71, 1) 100%);
+  background: linear-gradient(128deg, rgba(189, 255, 83, 1) 0%, rgba(43, 250, 82, 1) 100%);
+}
+
+.number-slide4 {
+  background: rgb(64, 255, 242);
+  background: linear-gradient(128deg, rgba(64, 255, 242, 1) 0%, rgba(63, 188, 255, 1) 100%);
+}
+
+.number-slide5 {
+  background: rgb(255, 64, 156);
+  background: linear-gradient(128deg, rgba(255, 64, 156, 1) 0%, rgba(255, 63, 63, 1) 100%);
+}
+.number-slide6 {
+  background: rgb(64, 76, 255);
+  background: linear-gradient(128deg, rgba(64, 76, 255, 1) 0%, rgba(174, 63, 255, 1) 100%);
+}
+</style>
