365b980a984ba6f38c88e23c5b0eda612070421e
cuzknothz <cuzknothz@gmail.com>
2022-11-01 21:12:41 +0700

    _

--- pages/_.vue
+++ pages/_.vue
@@ -6,7 +6,7 @@ import { IStory, ITimeLine } from '@@/models'
 import { useStoriesStore, useSuggestionStore, useTimeLineStore } from '@@/store'
 
 definePageMeta({
-  layout: 'main',
+    layout: 'main',
 })
 
 const storiesStore = useStoriesStore()
@@ -14,13 +14,13 @@ const timeLineStore = useTimeLineStore()
 const suggestionStore = useSuggestionStore()
 
 const { data: _timeline } = await useFetch<ITimeLine[]>(
-  'https://mocki.io/v1/546b9d75-01bc-4e88-a496-125656ead489'
+    'https://mocki.io/v1/dc96749b-96d4-4656-b8a5-83e6f4dc0fda'
 )
 const { data: _stories } = await useFetch<IStory[]>(
-  'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb'
+    'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb'
 )
 const { data: _suggestions } = await useFetch<IStory[]>(
-  'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb'
+    'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb'
 )
 
 storiesStore.save(_stories.value)
@@ -34,30 +34,30 @@ const suggestion = computed(() => suggestionStore.data)
 const rightRef = ref<HTMLElement>(null)
 
 const positionRight = ref(() => {
-  rightRef.value.style.top = `${window.scrollY}px`
+    rightRef.value.style.top = `${window.scrollY}px`
 })
 
 onMounted(() => {
-  positionRight.value()
-  addEventListener('scroll', positionRight.value)
+    positionRight.value()
+    addEventListener('scroll', positionRight.value)
 })
 
 onUnmounted(() => {
-  removeEventListener('scroll', positionRight.value)
+    removeEventListener('scroll', positionRight.value)
 })
 </script>
 
 <template>
-  <div class="dark:bg-black/20">
-    <div class="relative flex w-full justify-center lg:block">
-      <div class="inline-flex w-full flex-col items-center md:w-[614px] lg:block">
-        <Stories :stories="stories" />
-        <Post v-for="i in timeline" :key="i.id" v-bind="i" />
-      </div>
-      <div class="absolute top-0 right-0 hidden w-[293px] bg-white text-sm lg:block" ref="rightRef">
-        <Suggestions :suggestion="suggestion" />
-      </div>
+    <div class="dark:bg-black/20">
+        <div class="relative flex w-full justify-center lg:block">
+            <div class="inline-flex w-full flex-col items-center md:w-[614px] lg:block">
+                <Stories :stories="stories" />
+                <Post v-for="i in timeline" :key="i.id" v-bind="i" />
+            </div>
+            <div class="absolute top-0 right-0 hidden w-[293px] bg-white text-sm lg:block" ref="rightRef">
+                <Suggestions :suggestion="suggestion" />
+            </div>
+        </div>
+        <NuxtPage />
     </div>
-    <NuxtPage />
-  </div>
 </template>
--- pages/_/p/[id].vue
+++ pages/_/p/[id].vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-    <BackDrop>
-      <div
-        class="flex aspect-[1.5/1] w-[500px] flex-col bg-fuchsia-400 xl:h-[70vh] xl:w-auto xl:flex-row translate-y-[200%] opacity-0"
-        ref="postRef">
-        <div class="block h-[60px] border-b-[1px] border-black/20 xl:hidden"></div>
-        <div class="h-full bg-red-400 xl:w-[60%]"></div>
-        <div class="h-full bg-yellow-400 xl:w-[40%]">
-          <div class="hidden h-[60px] border-b-[1px] border-black/20 xl:block"></div>
-        </div>
-      </div>
-    </BackDrop>
-  </div>
+    <div>
+        <BackDrop>
+            <div class="flex aspect-[1.5/1] w-[500px] flex-col bg-fuchsia-400 xl:h-[70vh] xl:w-auto xl:flex-row translate-y-[200%] opacity-0"
+                ref="postRef">
+                <div class="block h-[60px] border-b-[1px] border-black/20 xl:hidden"></div>
+                <div class="h-full bg-red-400 xl:w-[60%]"></div>
+                <div class="h-full bg-yellow-400 xl:w-[40%]">
+                    <div class="hidden h-[60px] border-b-[1px] border-black/20 xl:block"></div>
+                </div>
+            </div>
+        </BackDrop>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -22,17 +21,16 @@ const postRef = ref<HTMLDivElement>(null)
 const router = useRouter()
 
 useClickOutSide(postRef, () => {
-  router.back()
+    router.back()
 })
 
 onMounted(() => {
-  gsap.to(postRef.value, {
-    translateY: 0,
-    ease: "elastic.out(1, 0.75)",
-    duration: 0.3,
-    opacity: 1
-  })
-
+    gsap.to(postRef.value, {
+        translateY: 0,
+        ease: "elastic.out(1, 0.75)",
+        duration: 0.3,
+        opacity: 1
+    })
 })
 </script>
 
