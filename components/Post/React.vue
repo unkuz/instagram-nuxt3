598ac8443effbb8ed5021772a9d27b6a0401b19a
cuzknothz <cuzknothz@gmail.com>
2022-11-03 13:03:20 +0700

    _

--- components/Post/React.vue
+++ components/Post/React.vue
@@ -8,10 +8,10 @@ import clsx from 'classnames'
 import UnlikeIcon from '../Tiny/UnlikeIcon.vue'
 
 interface IProps {
-  has_liked: boolean
-  currentIdx: number
-  mediaArr: any
-  id: any
+    has_liked: boolean
+    currentIdx: number
+    mediaArr: any
+    id: any
 }
 
 const { id } = defineProps<IProps>()
@@ -19,34 +19,38 @@ const viewPostDetailStore = useViewPostDetailStore()
 const timelineStore = useTimeLineStore()
 
 const viewPost = () => {
-  viewPostDetailStore.setPostDetail(id)
-  navigateTo(`/_/p/${id}`)
+    viewPostDetailStore.setPostDetail(id)
+    navigateTo(`/_/p/${id}`)
 }
 
 const toggleLike = () => {
-  timelineStore.setToggleLike(id)
+    timelineStore.setToggleLike(id)
 }
+
+
 </script>
 
 <template>
-  <div class="mt-[5px] grid h-[35px] grid-cols-3">
-    <div class="flex h-full w-full items-center space-x-[10px]">
-      <div @click="toggleLike">
-        <LikeIcon_ v-if="!has_liked" />
-        <UnlikeIcon v-else />
-      </div>
-      <ViewPostIcon_ @click="viewPost" />
-      <ShareIcon_ />
+    <div :class="clsx('mt-[5px] grid h-[35px] grid-cols-3', {
+        'grid-cols-2': mediaArr.length < 2
+    })">
+        <div class="flex h-full w-full items-center space-x-[10px]">
+            <div @click="toggleLike">
+                <LikeIcon_ v-if="!has_liked" />
+                <UnlikeIcon v-else />
+            </div>
+            <ViewPostIcon_ @click="viewPost" />
+            <ShareIcon_ />
+        </div>
+        <div v-if="mediaArr.length > 1" class="flex items-center justify-center space-x-[4px]">
+            <div v-for="(_i, idx) in mediaArr" :key="idx" :class="
+    clsx('h-[6px] w-[6px]  rounded-[50%]  bg-[#b9b9b9]', {
+        'bg-blue-500': currentIdx === idx,
+    })
+            "></div>
+        </div>
+        <div class="flex h-full w-full items-center justify-end">
+            <SaveIcon_ />
+        </div>
     </div>
-    <div class="flex items-center justify-center space-x-[4px]">
-      <div v-for="(_i, idx) in mediaArr" :key="idx" :class="
-  clsx('h-[6px] w-[6px]  rounded-[50%]  bg-[#b9b9b9]', {
-    'bg-blue-500': currentIdx === idx,
-  })
-      "></div>
-    </div>
-    <div class="flex h-full w-full items-center justify-end">
-      <SaveIcon_ />
-    </div>
-  </div>
 </template>
