256e3425b4b09d3a150a928536367694dbecdca2
cuzknothz <cuzknothz@gmail.com>
2022-11-06 13:23:40 +0700

    _

--- components/Stories/index.vue
+++ components/Stories/index.vue
@@ -5,72 +5,72 @@ import { useKeenSlider } from 'keen-slider/vue.es'
 import Story from './Story.vue'
 
 export default {
-  setup() {
-    const [container] = useKeenSlider({
-      breakpoints: {
-        '(min-width: 768px)': {
-          slides: {
-            perView: 7,
-          },
-        },
-        '(min-width: 600px) and (max-width: 767px)': {
-          slides: {
-            perView: 6.5,
-          },
-        },
-        '(min-width: 490px) and (max-width: 599px)': {
-          slides: {
-            perView: 6,
-          },
-        },
-        '(min-width: 433px) and (max-width: 489px)': {
-          slides: {
-            perView: 5.5,
-          },
-        },
-        '(min-width: 385px) and (max-width: 432px)': {
-          slides: {
-            perView: 5,
-          },
-        },
-        '(min-width: 310px) and (max-width: 384px)': {
-          slides: {
-            perView: 4,
-          },
-        },
-        '(min-width: 270px) and (max-width: 309px)': {
-          slides: {
-            perView: 3.5,
-          },
-        },
-        '(min-width: 0px) and (max-width: 269px)': {
-          slides: {
-            perView: 3,
-          },
-        },
-      },
-      slides: {
-        spacing: 0,
-      },
-    })
-    return { container }
-  },
-  props: {
-    stories: Array<IStory>,
-  },
-  components: {
-    Story,
-  },
+    setup() {
+        const [container] = useKeenSlider({
+            breakpoints: {
+                '(min-width: 768px)': {
+                    slides: {
+                        perView: 7,
+                    },
+                },
+                '(min-width: 600px) and (max-width: 767px)': {
+                    slides: {
+                        perView: 6.5,
+                    },
+                },
+                '(min-width: 490px) and (max-width: 599px)': {
+                    slides: {
+                        perView: 6,
+                    },
+                },
+                '(min-width: 433px) and (max-width: 489px)': {
+                    slides: {
+                        perView: 5.5,
+                    },
+                },
+                '(min-width: 385px) and (max-width: 432px)': {
+                    slides: {
+                        perView: 5,
+                    },
+                },
+                '(min-width: 310px) and (max-width: 384px)': {
+                    slides: {
+                        perView: 4,
+                    },
+                },
+                '(min-width: 270px) and (max-width: 309px)': {
+                    slides: {
+                        perView: 3.5,
+                    },
+                },
+                '(min-width: 0px) and (max-width: 269px)': {
+                    slides: {
+                        perView: 3,
+                    },
+                },
+            },
+            slides: {
+                spacing: 0,
+            },
+        })
+        return { container }
+    },
+    props: {
+        stories: Array<IStory>,
+    },
+    components: {
+        Story,
+    },
 }
 </script>
 
 <template>
-  <div
-    class="relative flex h-[119px] w-full items-center border-0 border-gray-200 shadow-gray-200 md:mb-[24px] md:border-[1px] md:shadow-sm">
-    <div ref="container" class="keen-slider h-full cursor-grabbing pt-[20px]">
-      <div class="keen-slider__slide" v-for="i in stories" :key="i.id">
-        <Story v-bind="i" />
-      </div>
+    <div
+        class="relative flex h-[119px] w-full items-center border-0 border-gray-200 shadow-gray-200 md:mb-[24px] md:border-[1px] md:shadow-sm">
+        <div ref="container" class="keen-slider h-full  pt-[20px]">
+            <div class="keen-slider__slide" v-for="i in stories" :key="i.id">
+                <Story v-bind="i" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
