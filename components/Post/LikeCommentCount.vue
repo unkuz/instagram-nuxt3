262d1b16de41eef203b1980a7b4b75507c43d3c1
wize-nguyen.cuong <nguyen.cuong@wize-net.co.jp>
2022-11-21 09:49:13 +0700

    _

--- components/Post/LikeCommentCount.vue
+++ components/Post/LikeCommentCount.vue
@@ -10,7 +10,7 @@ const { format } = Intl.NumberFormat('en', { notation: 'compact' })
 </script>
 
 <template>
-    <div class="mb-[5px] flex h-[18px] cursor-pointer gap-[5px] text-[0.8rem]">
+    <div class="mb-[5px] flex h-[18px] cursor-pointer gap-[5px] text-[0.85rem]">
         <span class="text-red-400"><span>{{ format(likeCount) }}</span> <span>likes</span></span>
         <span>|</span>
         <span class="text-fuchsia-400">
