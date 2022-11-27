870311d4bcf8beae99dfcb4de27b73d41af3a1f7
cuzknothz <cuzknothz@gmail.com>
2022-11-27 23:27:54 +0700

    _

--- components/Post/index.vue
+++ components/Post/index.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import IndividualComment from '@@/components/Post/IndividualComment.vue'
+import { useForceRenderTimer } from '@@/composables'
 import moment from 'moment'
 import Caption from './Caption.vue'
 import Carousel from './Carousel.vue'
@@ -6,11 +8,6 @@ import Comment from './Comment.vue'
 import Head from './Head.vue'
 import LikeCommentCount from './LikeCommentCount.vue'
 import React from './React.vue'
-import IndividualComment from '@@/components/Post/IndividualComment.vue';
-import { gsap } from 'gsap';
-import DownIcon_ from '@@/assets/svg/down_icon_458438i.svg';
-import clsx from 'classnames';
-import { useForceRenderTimer } from '@@/composables'
 
 export interface IProps {
     created_at: string
@@ -77,24 +74,10 @@ const currentIdx = ref<number>(0)
 const setCurrent = (value: number) => (currentIdx.value = value)
 const mediaArr = computed(() => props.carousel_media.images.concat(props.carousel_media.videos))
 
-
-const animatitonComment = () => {
-    gsap.to(commentRef.value, {
-        height: isShowComment.value ? 200 : 0,
-        duration: 0.2
-    })
-}
-
-const toggleShowComment = () => {
-    isShowComment.value = !isShowComment.value
-    animatitonComment()
-}
-
 const { key } = useForceRenderTimer()
 
 const scrollToComment = () => {
     isShowComment.value = true
-    animatitonComment()
 }
 
 
@@ -114,17 +97,7 @@ const scrollToComment = () => {
             <div class="m-[0px_0px_0px_0px] h-[18px] text-[0.8rem] text-gray-400" :key="key">
                 {{ moment(created_at).fromNow() }}
             </div>
-            <div @click="toggleShowComment" v-if="comments.length > 0"
-                class="w-full text-[0.8rem]  flex justify-center text-[#00d9ff] select-none cursor-pointer">
-                <span>
-                    <DownIcon_ :class="clsx('w-[16px] origin-center animate-pulse [&>path]:fill-[#00d9ff]', {
-                        'rotate-180-css': isShowComment
-                    })" />
-                </span>
-                <div v-if="!isShowComment">Expand comment</div>
-                <div v-else>Collapse comment</div>
-            </div>
-            <div class="max-h-[200px]  overflow-y-scroll overflow-x-hidden w-full mt-[5px]" ref="commentRef">
+            <div class="w-full mt-[5px]" ref="commentRef">
                 <IndividualComment v-for="(i, idx) in comments" :comment="i" :key="idx" />
             </div>
             <ClientOnly>
