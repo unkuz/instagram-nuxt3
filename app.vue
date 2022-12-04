9c9c592f8b78fb4b586e70c43a78466ead7f3ec2
cuzknothz <cuzknothz@gmail.com>
2022-12-05 00:33:14 +0700

    ⚡

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import Loading from '@@/components/Atom/Loading.vue'
 import NewPost from '@@/components/Huge/NewPost/index.vue'
 import More from '@@/components/Utils/More.vue'
 import Prelude from '@@/components/Utils/Prelude.vue'
-import { usePrelude, useScroll, useSignature } from '@@/composables'
+import { usePrelude, useResizeWindow, useScroll, useSignature } from '@@/composables'
 import { SECTION } from '@@/constants'
 import { registerSeviceWorkerPWA } from '@@/helpers'
 import { useGlobalStore, useMoreStore, useThemeStore } from '@@/store'
@@ -12,9 +12,6 @@ import clsx from 'classnames'
 if (process.client) {
   window.addEventListener('load', registerSeviceWorkerPWA)
 }
-
-useScroll()
-useSignature()
 
 const globalStore = useGlobalStore()
 const moreStore = useMoreStore()
@@ -25,6 +22,10 @@ const isDarkMode = computed<boolean>(() => themeStore.darkMode)
 const section = computed<SECTION>(() => globalStore.section)
 const isMobile = computed<boolean>(() => globalStore.getIsMobile)
 const isShowMore = computed<boolean>(() => moreStore.isShow)
+
+useScroll()
+useSignature()
+useResizeWindow()
 </script>
 
 <template>
