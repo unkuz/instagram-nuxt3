60a86fafc438d5a3e46aec9400861a5d5badfa3e
wize-nguyen.cuong <nguyen.cuong@wize-net.co.jp>
2022-12-06 13:21:14 +0700

    🥬

--- composables/useScrollBarTheme.ts
+++ composables/useScrollBarTheme.ts
@@ -2,33 +2,40 @@ import { useThemeStore } from '~~/store'
 import { useWatchWithMounted } from './useWatchWithMounted'
 
 export const useScrollBarTheme = () => {
+  const CUZ = '__cuzknothz'
   const themeStore = useThemeStore()
 
   const styleCss = computed(() => {
-    const { darkMode } = themeStore
     return `
-/* Custom ScrollBar Chrome Opera */
-::-webkit-scrollbar {
-    width: 6px;
-    height: 6px;
-}
 ::-webkit-scrollbar-track {
-  background: ${darkMode ? '#121212' : 'white'};
-}
-::-webkit-scrollbar-thumb {
-  background: #00b3ff;
-}
-::-webkit-scrollbar-thumb:hover {
-    background: #ff0088;
-}
-/* Custom ScrollBar FireFox */
-        `
+  background: #121212;
+}`
   })
 
+  const checkIsCurrentHasStyle = (headTag: HTMLHeadElement) => {
+    const lastElIdx = headTag.childNodes.length - 1
+    return headTag.childNodes[lastElIdx]?.localName === 'style' &&
+      headTag.childNodes[lastElIdx].attributes[0]?.name === CUZ
+      ? true
+      : false
+  }
+
   const makeStyle = () => {
-    let styleElement = document.createElement('style')
-    styleElement.appendChild(document.createTextNode(styleCss.value))
-    document.getElementsByTagName('head')[0].appendChild(styleElement)
+    const headTag = document.getElementsByTagName('head')[0]
+
+    if (themeStore.darkMode) {
+      let el = document.createElement('style')
+      el.setAttribute(CUZ, CUZ)
+      el.appendChild(document.createTextNode(styleCss.value))
+      headTag.appendChild(el)
+    } else {
+      if (checkIsCurrentHasStyle(headTag)) {
+        const elNeedRm = headTag.lastChild
+        if (elNeedRm) {
+          headTag.removeChild(elNeedRm)
+        }
+      }
+    }
   }
 
   useWatchWithMounted(() => themeStore.darkMode, makeStyle)
