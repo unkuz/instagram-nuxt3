40bbf455c72c87cc1638488131db02d30e51e362
wize-nguyen.cuong <nguyen.cuong@wize-net.co.jp>
2022-12-12 12:50:54 +0700

    🌲

--- apis/endpoint/index.ts
+++ apis/endpoint/index.ts
@@ -1,6 +1,6 @@
 export const APP_API = {
   timeLine: {
-    list: 'https://mocki.io/v1/ace10cdb-8b5f-4ee6-8b50-9fc3bf6e3cdc',
+    list: 'https://mocki.io/v1/db8ad347-55a8-4e30-8852-dc2e02603696',
   },
   stories: {
     list: 'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb',
