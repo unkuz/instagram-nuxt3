7e0dedb30bd92073d832119a4f8243ae943c0751
cuzknothz <cuzknothz@gmail.com>
2022-12-19 23:01:32 +0700

    🍪

--- components/Atoms/UnlikeIcon.vue
+++ components/Atoms/UnlikeIcon.vue
@@ -3,17 +3,22 @@ import UnlikeIcon_ from '@@/assets/svg/unlike_icon.svg'
 import { gsap } from 'gsap'
 
 const unlikeIconRef = ref<HTMLElement | null>(null)
+let tl = gsap.timeline()
 
 onMounted(() => {
-  const tl = gsap.timeline()
   tl.to(unlikeIconRef.value, {
     scale: 1.5,
     duration: 0.1,
   })
+
   tl.to(unlikeIconRef.value, {
     scale: 1,
     duration: 0.1,
   })
+})
+
+onBeforeUnmount(() => {
+  tl.kill()
 })
 </script>
 
