d40bb8ccaec922c68e1cdef3a7b7070958e674c1
cuzknothz <cuzknothz@gmail.com>
2022-12-24 11:14:56 +0700

    🙄

--- composables/useScrollBarTheme.ts
+++ composables/useScrollBarTheme.ts
@@ -26,19 +26,17 @@ export const useScrollBarTheme = () => {
     const headTag = document.getElementsByTagName('head')[0]
 
     if (themeStore.darkMode) {
-      let el = document.createElement('style')
+      const el = document.createElement('style')
       el.setAttribute('__cuzknothz', '__cuzknothz')
       el.appendChild(document.createTextNode(styleCss.value))
       headTag.appendChild(el)
-    } else {
-      if (checkIsCurrentHasStyle(headTag)) {
-        headTag.childNodes.forEach((i: any) => {
-          const { localName, attributes } = i
-          if (localName === 'style' && attributes[0]?.name === '__cuzknothz') {
-            return headTag.removeChild(i)
-          }
-        })
-      }
+    } else if (checkIsCurrentHasStyle(headTag)) {
+      headTag.childNodes.forEach((i: any) => {
+        const { localName, attributes } = i
+        if (localName === 'style' && attributes[0]?.name === '__cuzknothz') {
+          return headTag.removeChild(i)
+        }
+      })
     }
   }
 
