1810191d841bb394c3a62198ef6859f279d2f20f
wize-nguyen.cuong <nguyen.cuong@wize-net.co.jp>
2023-01-12 18:56:20 +0700

    🥕

--- apis/endpoint/index.ts
+++ apis/endpoint/index.ts
@@ -1,6 +1,6 @@
 export const APP_API = {
   timeLine: {
-    list: 'https://mocki.io/v1/56124414-4922-41f0-86a4-a68022991b2a',
+    list: 'https://mocki.io/v1/72c5ca40-8ffd-4728-a5b9-a66073e29c5c',
   },
   stories: {
     list: 'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb',
--- mocks/reelTimeLine.ts
+++ mocks/reelTimeLine.ts
@@ -1,5 +1,38 @@
 export const timeLine = [
   {
+    created_at: '2023-01-12 13:20',
+    id: '45gffdafsdsdffaddadfdfffddfddfs',
+    location: {
+      short_name: 'Copenhagen',
+    },
+    comments: [],
+    carousel_media: {
+      images: [],
+      videos: [
+        {
+          id: 2,
+          src: '/video/Y2Mate.is - KienThien & Allie Coss - Be With You-ZFqv3qbYLiE-720p-1654390315468.mp4',
+        },
+      ],
+    },
+    user: {
+      id: 'Diversity',
+      username: 'Diversity',
+      full_name: 'Diversity',
+      profile_pic_url: '/image/91480011.jpg',
+      friendship_status: {
+        following: true,
+        outgoing_request: false,
+      },
+    },
+    like_count: 4650,
+    has_liked: true,
+    caption_text: `KienThien & Allie Coss - Be With You`,
+    is_seen: true,
+    tags: ['KienThien', 'Diversity'],
+    is_saved: true,
+  },
+  {
     created_at: '2023-01-01 17:20',
     id: '45gffdafsdsdfffdfffddfddfs',
     location: {
@@ -1502,3 +1535,4 @@ APEX Records`,
     is_saved: true,
   },
 ]
+
