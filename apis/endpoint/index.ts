0dde96bfcb2c873eef3a5a450238cee33bf9aa32
cuzknothz <cuzknothz@gmail.com>
2023-01-15 21:25:08 +0700

    🥒

--- apis/endpoint/index.ts
+++ apis/endpoint/index.ts
@@ -1,6 +1,6 @@
 export const APP_API = {
   timeLine: {
-    list: 'https://mocki.io/v1/f804d155-5c81-4c8b-b01c-0d6901e81788',
+    list: 'https://mocki.io/v1/e9f9ac60-7cbe-40a7-be78-d603ddfa6a15',
   },
   stories: {
     list: 'https://mocki.io/v1/5e99de01-56f3-46a5-a0a0-477dcfd34beb',
--- mocks/reelTimeLine.ts
+++ mocks/reelTimeLine.ts
@@ -1,11 +1,25 @@
 export const timeLine = [
   {
     created_at: '2023-01-15 13:20',
-    id: '45gffdafsdsdffaddadfdfffddfddfs',
+    id: '283975891eytru',
     location: {
       short_name: 'Copenhagen',
     },
-    comments: [],
+    comments: [
+      {
+        text: 'My new track No Stopping Love is out now on NCS and I wanted to show you guys the lyric video of the track! Hope you like it!',
+        created_at: '2022-12-19 23:45',
+        user: {
+          pk: 20433830474,
+          username: 'DirtyPalm',
+          full_name: 'Dirty Palm',
+          is_private: false,
+          profile_pic_url: '/timeline/jksadf.jpg',
+        },
+        comment_like_count: 1,
+        reply: [],
+      },
+    ],
     carousel_media: {
       images: [],
       videos: [
@@ -27,10 +41,9 @@ export const timeLine = [
     },
     like_count: 4650,
     has_liked: true,
-    caption_text: `KienThien & Allie Coss - Be With You`,
+    caption_text: `Dirty Palm - No Stopping Love`,
     is_seen: true,
     tags: ['Dirty Palm', 'No Stopping Love'],
     is_saved: true,
   },
 ]
-
