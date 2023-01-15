c54534690c201259371e959bd96c2280d2f2ae98
cuzknothz <cuzknothz@gmail.com>
2023-01-15 23:12:04 +0700

    🍈

---
+++ pages/lotties.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Luv from '@@/lotties/lf20_5vy0an4n.json'
+</script>
+<template>
+  <div>
+    <ClientOnly>
+      <Vue3Lottie :animationData="Luv" class="w-full h-full"> </Vue3Lottie>
+    </ClientOnly>
+  </div>
+</template>
