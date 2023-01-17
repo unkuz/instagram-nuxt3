1c44e1a6e65c837177d6c37ebc2ad4640128789b
cuzknothz <cuzknothz@gmail.com>
2023-01-17 20:36:02 +0700

    🤨

--- components/Utils/Cookie.vue
+++ components/Utils/Cookie.vue
@@ -19,13 +19,16 @@ const acceptCookieUse = () => {
 onMounted(() => {
     tl.to(containerRef, {
         height: 'auto',
+        duration: 1,
+        bottom: '40px',
+        delay: 2,
     })
 })
 </script>
 <template>
     <div
         ref="containerRef"
-        class="fixed bottom-[40px] left-[40px] hidden h-0 w-[240px] rounded-[0.5rem] bg-c1 p-[40px_24px_24px_24px] shadow-md md:block"
+        class="fixed -bottom-[100px] left-[40px] hidden h-0 w-[240px] rounded-[0.5rem] bg-c1 p-[40px_24px_20px_24px] shadow-md md:block"
     >
         <CookieIcon_ class="absolute -top-[23px] right-1/2 translate-x-1/2" />
         <div class="text-[0.8rem]">
@@ -36,11 +39,11 @@ onMounted(() => {
             </p>
             <div class="mt-[10px] flex w-full items-center justify-between">
                 <NuxtLink to="/privacy-policy" class="cursor-pointer">
-                    <span @click="animate">Privacy Policy</span>
+                    <span class="text-c9" @click="animate">Privacy Policy</span>
                 </NuxtLink>
                 <Button
                     text="OK"
-                    class="select-none rounded-[0.25rem] bg-[#00ddb3] px-[32px] py-[8px] leading-[0.8rem] text-c1"
+                    class="select-none rounded-[0.25rem] bg-[#00ddb3] px-[32px] py-[8px] leading-[0.8rem] text-c1 hover:bg-c15"
                     @click="acceptCookieUse"
                 />
             </div>
