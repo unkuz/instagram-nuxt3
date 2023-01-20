c4684c883b4e7de108262365c41523cf118b175e
cuzknothz <cuzknothz@gmail.com>
2023-01-20 21:06:02 +0700

    😃

--- configs/app.ts
+++ configs/app.ts
@@ -3,6 +3,6 @@ export const QUALITY_IMAGE = Object.freeze({
     EXPLORE: 50,
 })
 
-export const MAX_SUGGESTION_PEOPLE_FOLLOW = 8
+export const MAX_SUGGESTION_PEOPLE_FOLLOW = 7
 
 export const GITHUB_AUTHOR_LINK = 'https://github.com/cuzknothz'
