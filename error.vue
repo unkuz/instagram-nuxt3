ccbcc6583b2b8ba3ac014b093c7bf53a490f7bfe
cuzknothz <cuzknothz@gmail.com>
2023-01-27 15:49:43 +0700

    🍢

--- error.vue
+++ error.vue
@@ -1,13 +1,15 @@
-<template>
-  <div
-    class="flex h-[calc(100vh-84px)] w-full items-center justify-center text-[0.9rem]"
-  >
-    <span @click="handleError">Oops Something went wrong. (╯°□°）╯︵ ┻━┻.</span>
-  </div>
-</template>
 <script setup lang="ts">
+import ErrorPage from './components/Utils/ErrorPage.vue'
+
 const props = defineProps({
   error: Object,
 })
 const handleError = () => clearError({ redirect: '/' })
 </script>
+<template>
+  <div
+    class="flex h-[100vh] w-screen items-center justify-center text-[0.85rem]"
+  >
+    <ErrorPage />
+  </div>
+</template>
