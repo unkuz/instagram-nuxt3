0d1e2b2b7505ba675c5fc3f9834459e40228f60e
cuzknothz <cuzknothz@gmail.com>
2023-01-27 16:13:10 +0700

    🍭

--- components/Utils/ErrorPage.vue
+++ components/Utils/ErrorPage.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Error from '@@/lotties/error.lotties.json'
+import Error_lottie from '@@/lotties/error.lotties.json'
 </script>
 <template>
   <div>
     <ClientOnly>
-      <Vue3Lottie :animation-data="Error" class="h-full w-full" />
+      <Vue3Lottie :animation-data="Error_lottie" class="h-full w-full" />
     </ClientOnly>
   </div>
 </template>
