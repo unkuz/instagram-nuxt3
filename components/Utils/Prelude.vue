9ae3f98fc8b80ddfa4c0fdf0bd0d085dec88d1de
cuzknothz <cuzknothz@gmail.com>
2023-01-27 12:22:47 +0700

    🍬

--- components/Utils/Prelude.vue
+++ components/Utils/Prelude.vue
@@ -8,7 +8,7 @@ useLockScroll()
 <template>
   <div
     v-once
-    class="fixed top-0 left-0 z-20 flex h-screen w-full flex-col items-center justify-center space-y-[50px] bg-c1 duration-75 dark:bg-c19"
+    class="fixed inset-0 z-20 flex flex-col items-center justify-center space-y-[50px] bg-c1 duration-75 dark:bg-c19"
   >
     <div class="w-[100px]">
       <Logo />
