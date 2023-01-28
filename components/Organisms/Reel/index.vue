12aac09ddc2231fa014d2a7d06866d5143e3a221
cuzknothz <cuzknothz@gmail.com>
2023-01-28 21:43:57 +0700

    🥩

--- components/Organisms/Reel/index.vue
+++ components/Organisms/Reel/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useKeenSlider } from 'keen-slider/vue.es'
 
-const [container] = useKeenSlider({
+const [container, slider] = useKeenSlider({
   slides: {
     perView: 1,
   },
@@ -11,6 +11,28 @@ const [container] = useKeenSlider({
   defaultAnimation: {
     duration: 1,
   },
+})
+
+const videoRefs = $ref<HTMLVideoElement[]>([])
+
+onMounted(() => {
+  document.onkeydown = (e) => {
+    switch (String(e.key).toLowerCase()) {
+      case 'arrowup':
+        slider.value!.prev()
+        break
+      case 'arrowdown':
+        slider.value!.next()
+        break
+      case 'm':
+        console.log('M')
+        // left arrow
+        break
+      case 'l':
+        console.log('L')
+      // right arrow
+    }
+  }
 })
 </script>
 
@@ -22,9 +44,10 @@ const [container] = useKeenSlider({
     <div
       v-for="(_i, idx) in 2"
       :key="idx"
-      class="keen-slider__slide !w-auto cursor-grab active:cursor-grabbing"
+      class="keen-slider__slide !w-auto cursor-grab duration-500 active:cursor-grabbing"
     >
       <video
+        ref="videoRefs"
         class="h-[calc(100vh-65px)] w-auto object-cover md:h-[calc(100vh-114px)]"
         src="/video/242322324_812426336116660_1281566458903572911_n.mp4"
         autoplay
