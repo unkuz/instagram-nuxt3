77c5145f07651b93bb05f73f57b67f8134dfd47c
cuzknothz <cuzknothz@gmail.com>
2023-01-28 20:24:09 +0700

    🍬

--- components/Organisms/Reel/index.vue
+++ components/Organisms/Reel/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { useKeenSlider } from 'keen-slider/vue.es';
-
+import { useKeenSlider } from 'keen-slider/vue.es'
 
 const [container] = useKeenSlider({
   slides: {
@@ -9,6 +8,9 @@ const [container] = useKeenSlider({
   dragSpeed: 0.3,
   renderMode: 'precision',
   vertical: true,
+  defaultAnimation: {
+    duration: 1,
+  },
 })
 </script>
 
@@ -17,9 +19,9 @@ const [container] = useKeenSlider({
     ref="container"
     class="keen-slider flex flex-col !flex-nowrap items-center"
   >
-    <div v-for="(_i, idx) in 2" :key="idx" class="keen-slider__slide !w-auto">
+    <div v-for="(_i, idx) in 2" :key="idx" class="keen-slider__slide !w-auto cursor-grab active:cursor-grabbing">
       <video
-        class="h-[calc(100vh-84px)] w-auto object-cover"
+        class="md:h-[calc(100vh-84px)] h-[calc(100vh-65px)] w-auto object-cover"
         src="/video/242322324_812426336116660_1281566458903572911_n.mp4"
         autoplay
         loop
