dae6a87d0c9186cea73013e40b028ccb4bce687a
cuzknothz <cuzknothz@gmail.com>
2023-01-28 12:47:39 +0700

    🍭

--- components/Atoms/TagName.vue
+++ components/Atoms/TagName.vue
@@ -9,7 +9,7 @@ withDefaults(defineProps<IProps>(), {
 </script>
 <template>
   <div
-    class="inline-block bg-c2 px-[8px] md:py-[1px] py-[2px] text-c1 shadow-md dark:bg-c1 dark:text-c2 md:text-[.875rem] text-[.8rem]"
+    class="inline-block bg-c2 px-[8px] py-[4px] text-[.8rem] leading-[.8rem] text-c1 shadow-md dark:bg-c1 dark:text-c2 md:py-[4px] md:text-[.875rem] md:leading-[.875rem]"
   >
     {{ name }}
   </div>
