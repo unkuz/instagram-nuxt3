e08b8128bc67bb67d1ada02bd57fb1a69acf0830
cuzknothz <cuzknothz@gmail.com>
2023-01-28 22:35:10 +0700

    🍭

--- components/Organisms/Reel/index.vue
+++ components/Organisms/Reel/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useKeenSlider } from 'keen-slider/vue.es'
+import ReelKeyBoardShortcut from '@@/components/Utils/ReelKeyBoardShortcut.vue'
 
 const [container, slider] = useKeenSlider({
   slides: {
@@ -70,27 +71,30 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div
-    ref="container"
-    class="keen-slider flex h-[calc(100vh-65px)] flex-col !flex-nowrap items-center md:h-[calc(100vh-84px)]"
-  >
+  <div>
     <div
-      v-for="(_i, idx) in 2"
-      :key="idx"
-      class="keen-slider__slide !w-auto cursor-grab duration-500 active:cursor-grabbing"
+      ref="container"
+      class="keen-slider flex h-[calc(100vh-65px)] flex-col !flex-nowrap items-center md:h-[calc(100vh-84px)]"
     >
-      <video
-        ref="videoRefs"
-        class="h-[calc(100vh-65px)] w-auto object-cover md:h-[calc(100vh-114px)]"
-        src="/video/242322324_812426336116660_1281566458903572911_n.mp4"
-        autoplay
-        loop
-        muted
-        type="video/mp4"
-        playsinline
-        crossorigin="anonymous"
-        preload="metadata"
-      />
+      <div
+        v-for="(_i, idx) in 2"
+        :key="idx"
+        class="keen-slider__slide !w-auto cursor-grab duration-500 active:cursor-grabbing"
+      >
+        <video
+          ref="videoRefs"
+          class="h-[calc(100vh-65px)] w-auto object-cover md:h-[calc(100vh-114px)]"
+          src="/video/242322324_812426336116660_1281566458903572911_n.mp4"
+          autoplay
+          loop
+          muted
+          type="video/mp4"
+          playsinline
+          crossorigin="anonymous"
+          preload="metadata"
+        />
+      </div>
     </div>
+    <ReelKeyBoardShortcut />
   </div>
 </template>
---
+++ components/Utils/ReelKeyBoardShortcut.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import KeyboardIcon_ from '@@/assets/svg/keyboard.svg'
+import Button from '@@/components/Atoms/Button.vue'
+import KbUpIcon_ from '@@/assets/svg/kb_up.svg'
+import KbDownIcon_ from '@@/assets/svg/kb_down.svg'
+import KbLIcon_ from '@@/assets/svg/kb_l.svg'
+import KbMIcon_ from '@@/assets/svg/kb_m.svg'
+
+const intructionMap = [
+  { icon: KbUpIcon_, content: 'Go to previous video' },
+  { icon: KbDownIcon_, content: 'Go to next video' },
+  { icon: KbLIcon_, content: 'Like / Unlike video' },
+  { icon: KbMIcon_, content: 'Mute / Unmute video ' },
+]
+</script>
+<template>
+  <div
+    ref="containerRef"
+    :class="[
+      'fixed right-0 cursor-help bottom-[40px] rounded-[0.5rem] bg-c1 p-[40px_24px_20px_24px]  shadow-md  dark:bg-c2 dark:text-c1   md:right-[40px]  md:w-[290px]',
+    ]"
+  >
+    <KeyboardIcon_
+      class="absolute -top-[23px] right-1/2 w-[65px] origin-center translate-x-1/2 -rotate-[20deg]"
+    />
+    <div class="text-[0.8rem]">
+      <p class="mt-[5px] text-center text-[.85rem] font-[600]">
+        {{ 'Introducing keyboard shortcuts!' }}
+      </p>
+      <p
+        class="mt-[5px] flex items-center gap-[20px] first:mt-[10px]"
+        v-for="({ icon, content }, idx) in intructionMap"
+        :key="idx"
+      >
+        <span><component :is="icon" /></span>{{ content }}
+      </p>
+
+      <div class="flex w-full justify-end">
+        <Button
+          text="OK"
+          class="select-none bg-c15 px-[20px] py-[6px] text-[0.8rem] text-c1 duration-500"
+        />
+      </div>
+    </div>
+  </div>
+</template>
