36a44faf5accf3084e781bf2a028555f95334990
cuzknothz <cuzknothz@gmail.com>
2023-01-28 22:42:49 +0700

    🍗

--- components/Utils/Cookie.vue
+++ components/Utils/Cookie.vue
@@ -37,7 +37,7 @@ useWindowResizeCallback(animateIn, false)
   <div
     ref="containerRef"
     :class="[
-      'fixed -bottom-[200px] left-[20px] right-[20px] rounded-[0.5rem] bg-c1 p-[40px_24px_20px_24px]  shadow-md  dark:bg-c2 dark:text-c1  md:left-[40px] md:right-[40px]  md:w-[290px]',
+      'fixed -bottom-[200px] left-[20px] right-[20px] rounded-[0.5rem] bg-c1 p-[40px_24px_20px_24px] shadow-md  duration-1000  dark:bg-c2 dark:text-c1  md:left-[40px] md:right-[40px]  md:w-[290px]',
     ]"
   >
     <CookieIcon_ class="absolute -top-[23px] right-1/2 translate-x-1/2" />
