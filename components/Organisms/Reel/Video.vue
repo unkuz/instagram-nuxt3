6ac050e0e52c6abeacf7e2a203633bb775db9df7
cuzknothz <cuzknothz@gmail.com>
2023-01-29 15:34:33 +0700

    🍔

---
+++ components/Organisms/Reel/Video.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface IProps {
+  src: string
+}
+defineProps<IProps>()
+</script>
+
+<template>
+  <video
+    ref="videoRefs"
+    class="h-[calc(100vh-65px)] md:h-[calc(100vh-114px)] md:max-w-[60vw] xl:max-w-[50vw] 2xl:max-w-[40vw]"
+    :src="src"
+    autoplay
+    loop
+    type="video/mp4"
+    playsinline
+    crossorigin="anonymous"
+    preload="metadata"
+  />
+</template>
