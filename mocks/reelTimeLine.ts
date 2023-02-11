0e4b5e83b6b4637a9b5e069033161034eef8e1a3
cuzknothz <cuzknothz@gmail.com>
2023-02-11 09:48:35 +0700

    🍕

--- mocks/reelTimeLine.ts
+++ mocks/reelTimeLine.ts
@@ -1,5 +1,44 @@
 export const timeLine = [
   {
+    created_at: '2023-02-11 10:20',
+    id: '32er',
+    location: {
+      short_name: 'Copenhagen',
+    },
+    comments: [],
+    carousel_media: {
+      images: [
+        {
+          id: 1,
+          src: `/video/296156517_439234241550335_9075911159131574339_n.jpg`,
+        },
+      ],
+      videos: [],
+    },
+    user: {
+      id: 'olivur.morris',
+      username: 'olivur.morris',
+      full_name: 'olivur.morris',
+      profile_pic_url: '/image/326386081_877905256589336_3572849433668934905_n.jpg',
+      friendship_status: {
+        following: true,
+        outgoing_request: false,
+      },
+    },
+    like_count: 2700,
+    has_liked: false,
+    caption_text: `Feels good to be home! 🔥
+    
+        After a few days of relaxation I hit the ground running this morning with work and spent my lunchtime editing my photos from my trip! 👊🏻
+        
+        I've got a lot of branded content to work on over the next couple of days and even more emails to respond to! 🙌🏻
+        
+        How's everyone else's week going? ✌🏻`,
+    is_seen: true,
+    tags: ['thursdaymotivation', 'ultrawide', 'setuptour', 'deskdecor', 'devlife'],
+    is_saved: false,
+  },
+  {
     created_at: '2023-02-10 21:20',
     id: '45gffdafffdfdddds',
     location: {
@@ -106,226 +145,6 @@ export const timeLine = [
     ✏️ For business inquiries and other issues please contact: hello@divergemusic.group`,
     is_seen: true,
     tags: ['Diversity', 'KienThien', 'Turn It Up'],
-    is_saved: false,
-  },
-  {
-    created_at: '2022-12-31 10:20',
-    id: '45gffdafffddds',
-    location: {
-      short_name: 'Copenhagen',
-    },
-    comments: [],
-    carousel_media: {
-      images: [],
-      videos: [
-        {
-          id: 1,
-          src: `/video/Crystal Rock x Anastasia Rose x Lucky Twice - Lucky.mp4`,
-          poster: '/video/poster/hehe.png',
-        },
-      ],
-    },
-    user: {
-      id: 'Diversity',
-      username: 'Diversity',
-      full_name: 'Diversity',
-      profile_pic_url: '/image/91480011.jpg',
-      friendship_status: {
-        following: true,
-        outgoing_request: false,
-      },
-    },
-    like_count: 2700,
-    has_liked: false,
-    caption_text: `🔊 Lucky by Lucky Twice, Crystal Rock, Anastasia Rose
-✨ Follow Diversity's Playlists on Spotify:
-➥ https://divr.link/SpotifyPlaylists
-
-🎧 Stream/Download:
-➥ https://lnk.to/LuckyRRT
-
-🎵 Genre: House
-Playlist: http://diversity.moe/House
-
-🌸 Crystal Rock
-https://www.instagram.com/crystal_rock/
-https://www.facebook.com/CrystalRockO...
-
-🌸 Anastasia Rose
-https://www.facebook.com/AnastasiaRos...
-https://www.instagram.com/anastasia_r...
-
-💽 Label: SME
-
-✏️ Lyrics
-I'm so lucky lucky
-I'm so lucky lucky
-I'm so lovely lovely
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-I'm so lucky lucky
-I'm so lucky lucky
-I'm so lovely lovely
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-I'm so lucky lucky
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-Even though you said
-It would never end it's over
-You were smiling on my arm
-Now you're crying on my shoulder
-
-There's nothing you can do
-To keep it out
-There's nothing you can do
-Just scream and shout, saying:
-
-I'm so lucky lucky
-I'm so lucky lucky
-I'm so lovely lovely
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-I'm so lucky lucky
-I'm so lucky lucky
-I'm so lovely lovely
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-I'm so lucky lucky
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-I'm so lucky lucky
-I'm so lucky lucky
-I'm so lovely lovely
-I'm so lovely lovely
-
-You can fool yourself
-I promise it will help
-Now every single day
-I just wanna hear you saying
-
-🎨 Artwork by ohisashiburi
-https://www.pixiv.net/users/1079073
-
-⭐️ Diversity
-http://diversity.moe/follow
-
-📄 Submit your music:
-http://diversity.moe/demo
-
-✏️ For business inquiries and other issues please contact: hello@divergemusic.group
-`,
-    is_seen: true,
-    tags: ['Diversity', 'Crystal Rock', 'Anastasia Rose', 'Lucky Twice'],
-    is_saved: false,
-  },
-  {
-    created_at: '2022-02-08 21:20',
-    id: '45gffdafffdfddddsdf',
-    location: {
-      short_name: 'Copenhagen',
-    },
-    comments: [],
-    carousel_media: {
-      images: [],
-      videos: [
-        {
-          id: 1,
-          src: `/video/Gotez - Talking Head.mp4`,
-          poster: '',
-        },
-      ],
-    },
-    user: {
-      id: 'Diversity',
-      username: 'Diversity',
-      full_name: 'Diversity',
-      profile_pic_url: '/image/91480011.jpg',
-      friendship_status: {
-        following: true,
-        outgoing_request: false,
-      },
-    },
-    like_count: 1900,
-    has_liked: false,
-    caption_text: `Stream/Download:
-    ➥ https://spinninrecords.com/releases/v...
-    
-    🎧 Genre: Future Bounce
-    
-    Gotez
-    • https://soundcloud.com/gotezmusic
-    • https://www.instagram.com/gotezmusic/
-    • https://open.spotify.com/artist/0F1Cw...
-    • https://www.facebook.com/gotezreal/
-    
-    💽 Label: Spinnin' Records
-    • https://open.spotify.com/user/spinnin...
-    • https://soundcloud.com/spinninrecords
-    • https://facebook.com/SpinninRecords
-    • https://instagram.com/spinninrecords
-    • https://twitter.com/SpinninRecords
-    • https://spinninrecords.com
-    
-    
-    
-    Artwork ✍ by Sakimichan
-    •  https://www.patreon.com/sakimichan
-    • https://www.pixiv.net/member.php?id=3... 
-    • http://sakimichan.deviantart.com/ 
-    • http://www.facebook.com/Sakimichanart 
-    • http://sakimichan.tumblr.com/ 
-    • http://www.artstation.com/artist/saki... 
-    • http://sakimichan.artstation.com/
-    
-    
-    
-    Diversity
-    • http://diversity.moe
-    • http://diversity.moe/youtube
-    • http://diversity.moe/facebook
-    • http://diversity.moe/instagram
-    • http://diversity.moe/twitter
-    • http://diversity.moe/pixiv
-    • http://diversity.moe/spotify
-    • http://diversity.moe/soundcloud
-    
-    📄 Submit your music:
-    • http://diversity.moe/demo
-    
-    ✏️ For business inquiries and other issues please contact: hello@divergemusic.group`,
-    is_seen: true,
-    tags: ['Gotez', 'Talking Head'],
     is_saved: false,
   },
 ]
