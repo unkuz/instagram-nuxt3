d809d79e2ac8f03cd2e508fe54af270d28159f49
cuzknothz <cuzknothz@gmail.com>
2023-02-11 11:38:31 +0700

    🥚

--- components/Utils/BackDrop.vue
+++ components/Utils/BackDrop.vue
@@ -5,7 +5,7 @@ import { APP_CONFIGS } from '~~/configs'
 <template>
   <div
     :class="[
-      'fixed top-0 left-0 z-20 flex h-screen w-screen items-center justify-center bg-c2/60',
+      'fixed top-0 left-0 z-20 flex h-screen w-screen items-center justify-center bg-c2/20',
       {
         'backdrop-blur-[1.5px]': APP_CONFIGS.BACK_DROP_BLUR,
       },
