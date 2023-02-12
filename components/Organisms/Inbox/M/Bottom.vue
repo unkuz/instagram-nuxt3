3823da94374b676cffd3221bdc7ac069bc1d42e5
cuzknothz <cuzknothz@gmail.com>
2023-02-12 10:19:11 +0700

    🍕

--- components/Organisms/Inbox/M/Bottom.vue
+++ components/Organisms/Inbox/M/Bottom.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
 import CameraFillIcon_ from '@/assets/svg/mingcute/camera_fill.svg'
-import PictureFillIcon_ from '@/assets/svg/mingcute/picture_fill.svg'
-import MicFillIcon_ from '@/assets/svg/mingcute/mic_fill.svg'
 import EmojiFillIcon_ from '@/assets/svg/mingcute/emoji_fill.svg'
 import LikeFillIcon_ from '@/assets/svg/mingcute/like_fill.svg'
+import MicFillIcon_ from '@/assets/svg/mingcute/mic_fill.svg'
+import PictureFillIcon_ from '@/assets/svg/mingcute/picture_fill.svg'
+import { useFocus } from '@vueuse/core'
+import { gsap } from 'gsap'
 import Emoji from '~~/components/Utils/Emoji.vue'
-import { onClickOutside } from '@vueuse/core'
 
 let showEmoji = $ref(false)
 const emojiRef = ref<HTMLDivElement>()
+const aniInputRef = $ref<HTMLSpanElement>()
+const inputRef = ref<HTMLInputElement>()
+const { focused } = useFocus(inputRef, { initialValue: false })
 
 const toggleEmoji = () => (showEmoji = !showEmoji)
 
+let tl = gsap.timeline({})
+
+watch(focused, (focused) => {
+  if (focused) {
+    tl.to(aniInputRef!, {
+      width: '100%',
+    })
+  } else {
+    tl.to(aniInputRef!, {
+      width: '0%',
+    })
+  }
+})
+
+onBeforeUnmount(() => {
+  tl.kill()
+})
 // onClickOutside(emojiRef, () => (showEmoji = true))
 </script>
 
@@ -27,11 +48,13 @@ const toggleEmoji = () => (showEmoji = !showEmoji)
       <span><CameraFillIcon_ /></span>
       <span><PictureFillIcon_ /></span>
       <span><MicFillIcon_ /></span>
-      <div class="relative h-[36px] w-[60%] overflow-hidden rounded-[18px] bg-c11 px-[10px]">
-        <span
+      <div class="relative h-[36px] w-[60%] overflow-hidden rounded-[18px] border-[1px] border-c4">
+        <span ref="aniInputRef" class="absolute h-full w-[0px] bg-c4"></span>
+        <span class="absolute inset-0 px-[10px]"
           ><input
+            ref="inputRef"
             type="text"
-            class="h-full w-full bg-transparent outline-none placeholder:pl-[10px] active:outline-none"
+            class="h-full w-full outline-none active:outline-none"
             placeholder="Message"
         /></span>
 
