349aac61f6c45b80426c43a5f71cd7167132fbf3
cuzknothz <cuzknothz@gmail.com>
2023-02-13 00:55:16 +0700

    🍢

--- components/Molecules/Stories/Story.vue
+++ components/Molecules/Stories/Story.vue
@@ -21,10 +21,8 @@ defineProps<IProps>()
     >
       <div
         :class="[
-          'mx-auto flex aspect-square h-[66px] w-[66px] flex-col items-center overflow-hidden rounded-[50%] bg-gradient-to-bl from-c17 to-c18 p-[3px] dark:from-c24 dark:to-c25',
-          {
-            '!from-[#a3a3a3] !to-[#a5a5a5] dark:!from-[#a3a3a3] dark:!to-[#a5a5a5]': hasRead,
-          },
+          'mx-auto flex aspect-square h-[66px] w-[66px] flex-col items-center overflow-hidden rounded-[50%]  p-[3px] ',
+          hasRead ? 'bg-c21/60' : 'bg-gradient-to-bl from-c17 to-c18 dark:from-c24 dark:to-c25',
         ]"
       >
         <div class="h-full w-full rounded-[50%] bg-c1 p-[2px] dark:bg-c19">
