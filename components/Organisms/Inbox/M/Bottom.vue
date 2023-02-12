7f2383ee76bc7f9a738bff3ae180ca3ba83ba46c
cuzknothz <cuzknothz@gmail.com>
2023-02-12 09:28:04 +0700

    🥩

--- components/Organisms/Inbox/M/Bottom.vue
+++ components/Organisms/Inbox/M/Bottom.vue
@@ -1,28 +1,41 @@
 <script setup lang="ts">
 import CameraFillIcon_ from '@/assets/svg/mingcute/camera_fill.svg'
 import PictureFillIcon_ from '@/assets/svg/mingcute/picture_fill.svg'
-
 import MicFillIcon_ from '@/assets/svg/mingcute/mic_fill.svg'
-
 import EmojiFillIcon_ from '@/assets/svg/mingcute/emoji_fill.svg'
-
 import LikeFillIcon_ from '@/assets/svg/mingcute/like_fill.svg'
+import Emoji from '~~/components/Utils/Emoji.vue'
+
+let showEmoji = $ref(false)
+
+const toggleEmoji = () => (showEmoji = !showEmoji)
 </script>
 
 <template>
-  <div
-    class="absolute bottom-0 right-0 left-0 flex h-[60px] items-center justify-between bg-black/80 p-[5px] [&__svg]:w-[22px] [&__svg]:text-blue-400"
-  >
-    <span><CameraFillIcon_ /></span>
-    <span><PictureFillIcon_ /></span>
-    <span><MicFillIcon_ /></span>
-    <div class="relative w-[60%] bg-red-50">
-      Input
+  <div class=" ">
+    <div class="relative"><Emoji :top="-378" class="!left-0" /></div>
 
-      <span class="absolute right-0 bottom-1/2 translate-y-1/2"><EmojiFillIcon_ /></span>
-    </div>
-    <div>
-      <LikeFillIcon_ />
+    <div
+      class="absolute bottom-0 right-0 left-0 flex h-[60px] items-center justify-between bg-black/80 p-[5px] [&__svg]:w-[22px] [&__svg]:cursor-pointer [&__svg]:text-blue-400"
+    >
+      <span><CameraFillIcon_ /></span>
+      <span><PictureFillIcon_ /></span>
+      <span><MicFillIcon_ /></span>
+      <div class="relative h-[30px] w-[60%] overflow-hidden rounded-[15px] bg-c1 px-[10px]">
+        <span
+          ><input
+            type="text"
+            class="h-full w-full bg-transparent outline-none placeholder:pl-[10px] active:outline-none"
+            placeholder="Message"
+        /></span>
+
+        <span @click="toggleEmoji" class="absolute right-[10px] bottom-1/2 translate-y-1/2"
+          ><EmojiFillIcon_ />
+        </span>
+      </div>
+      <div>
+        <LikeFillIcon_ />
+      </div>
     </div>
   </div>
 </template>
--- components/Utils/Emoji.vue
+++ components/Utils/Emoji.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import { EMOJI } from '@/constants'
 import { gsap, Elastic } from 'gsap'
+interface IProps {
+  top: number
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  top: -310,
+})
 
 defineEmits(['emoji-add'])
 
@@ -11,7 +18,7 @@ let tl: TimelineLite = gsap.timeline({})
 onMounted(() => {
   tl.to(el!, {
     height: 300,
-    top: -310,
+    top: props.top,
     duration: 0.3,
     ease: Elastic.easeOut.config(1, 0.6),
   })
@@ -23,24 +30,22 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div>
-    <div
-      ref="el"
-      class="absolute top-0 -left-[17px] h-0 w-screen overflow-y-scroll border-[1px] border-c4 bg-c1 dark:border-c20 dark:bg-c19 sm:w-auto"
-    >
-      <div v-for="({ label, icons }, idx) in EMOJI" :key="idx">
-        <p class="mt-[10px] h-[20px] pl-[10px] text-[0.8rem] font-[500]">
-          {{ label }}
-        </p>
-        <div class="grid w-full grid-cols-8 text-[1.5rem] sm:w-[280px] sm:grid-cols-7">
-          <div
-            v-for="(icon, idx) in icons"
-            :key="idx"
-            class="flex h-[40px] cursor-pointer select-none items-center justify-center"
-            @click="$emit('emoji-add', icon)"
-          >
-            {{ icon }}
-          </div>
+  <div
+    ref="el"
+    class="absolute top-0 -left-[17px] h-0 w-screen overflow-y-scroll border-[1px] border-c4 bg-c1 dark:border-c20 dark:bg-c19 sm:w-auto"
+  >
+    <div v-for="({ label, icons }, idx) in EMOJI" :key="idx">
+      <p class="mt-[10px] h-[20px] pl-[10px] text-[0.8rem] font-[500]">
+        {{ label }}
+      </p>
+      <div class="grid w-full grid-cols-8 text-[1.5rem] sm:w-[280px] sm:grid-cols-7">
+        <div
+          v-for="(icon, idx) in icons"
+          :key="idx"
+          class="flex h-[40px] cursor-pointer select-none items-center justify-center"
+          @click="$emit('emoji-add', icon)"
+        >
+          {{ icon }}
         </div>
       </div>
     </div>
