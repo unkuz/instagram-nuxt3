b8869aa32df8b5a48d691dab8e36f56570e9d57b
cuzknothz <cuzknothz@gmail.com>
2023-02-13 23:46:03 +0700

    🍌

--- components/Organisms/Inbox/List/IndividualLine.vue
+++ components/Organisms/Inbox/List/IndividualLine.vue
@@ -18,22 +18,22 @@ const goDetail = () => navigateTo('/inbox/5723987')
 <template>
   <div
     @click="goDetail"
-    class="flex w-full cursor-pointer items-center justify-between rounded-2xl p-[10px] duration-1000 hover:bg-c22"
+    class="flex w-full cursor-pointer items-center justify-between rounded-2xl p-[10px] duration-1000 hover:bg-c22 dark:hover:bg-c20"
   >
-    <div class="flex items-center gap-[5px]">
+    <div class="flex items-center gap-[10px]">
       <Avatar class="h-[60px] w-[60px]" :url="profile_pic_url" />
-      <div class="flex flex-col gap-[10px]">
+      <div class="flex flex-col gap-[5px]">
         <TagName name="cuzknothz" />
-        <span class="text-c19 line-clamp-1">{{ preview_lastest_message }}</span>
+        <span class="text-c19 line-clamp-1 dark:text-c1">{{ preview_lastest_message }}</span>
       </div>
     </div>
-    <div class="flex h-full flex-col items-end gap-[10px]">
+    <div class="flex h-full flex-col items-end gap-[8px]">
       <div>
         <TimeFromNow class="text-[.75rem]" :time="created_at" />
       </div>
       <div>
         <div
-          class="flex h-[22px] w-[22px] items-center justify-center rounded-[50%] bg-c12 text-[.7rem] font-[600]"
+          class="flex h-[22px] w-[22px] items-center justify-center rounded-[50%] bg-c14 text-[.7rem] font-[600]"
         >
           {{ message_not_seen_count }}
         </div>
