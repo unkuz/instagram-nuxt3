d4aeb6a666963e950cac932820d593bc598cb3d7
cuzknothz <cuzknothz@gmail.com>
2023-03-01 21:23:23 +0700

    🍬

--- pages/[user]/index.vue
+++ pages/[user]/index.vue
@@ -77,12 +77,7 @@ onBeforeUnmount(()=>{
       </div> -->
     </div>
     <div class="mt-[20px] grid grid-cols-3 gap-[5px]">
-      <div class="overflow-hidden">
-        <nuxt-img
-          src="/personal/284244344_5311122635605193_5864440318105391567_n.jpg"
-          class="aspect-square w-full cursor-pointer overflow-hidden object-cover hover:scale-110"
-        />
-      </div>
+   
     </div>
     <!-- <Modal v-if="isShowFollowing" /> -->
   </div>
