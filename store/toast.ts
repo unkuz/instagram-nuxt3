4e2da43654c7bd06f203b6ab11e88ae6f1dc5cbc
cuzknothz <cuzknothz@gmail.com>
2023-03-04 20:28:06 +0700

    😀

--- store/toast.ts
+++ store/toast.ts
@@ -2,16 +2,15 @@ import { defineStore } from 'pinia'
 
 export const useToastStore = defineStore('toast', {
   state: () => ({
-    isShow: false,
+    isShow: true,
   }),
   getters: {},
   actions: {
     toggle(val: boolean) {
       if (_isBoolean(val)) {
         let timer: NodeJS.Timer | '' = ''
+        this.isShow = val
         if (val) {
-          this.isShow = val
-
           timer = setTimeout(() => (this.isShow = false), 1000)
         } else {
           clearTimeout(timer)
