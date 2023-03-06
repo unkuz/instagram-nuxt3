8b05cb7367a09977c2b9649f8f506e2b7608b8e4
cuzknothz <cuzknothz@gmail.com>
2023-03-06 17:13:36 +0700

    🧉

--- components/Atoms/Avatar.vue
+++ components/Atoms/Avatar.vue
@@ -29,6 +29,6 @@ withDefaults(defineProps<IProps>(), {
     ]"
   >
     <div v-if="skeleton" class="skeleton h-full w-full"></div>
-    <nuxt-img v-else :class="['object-cover']" :src="url" :quality="quality" />
+    <nuxt-img v-else :class="['h-full w-full object-cover']" :src="url" :quality="quality" />
   </div>
 </template>
