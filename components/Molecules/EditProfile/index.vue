cd71d2158392f995f4bf18e7d42976578a8763a4
cuzknothz <cuzknothz@gmail.com>
2023-03-22 21:13:35 +0700

    🥕

---
+++ components/Molecules/EditProfile/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Avatar from '~~/components/Atoms/Avatar.vue'
+import { useProfileStore } from '~~/store'
+import { SizeAvatarEnum } from '~~/type'
+
+const profileStore = useProfileStore()
+
+const profile = $computed(() => profileStore.profile)
+</script>
+<template></template>
--- pages/[user]/index.vue
+++ pages/[user]/index.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
+import { APP_API } from '@/apis'
 import Avatar from '@/components/Atoms/Avatar.vue'
 import TagName from '@/components/Atoms/TagName.vue'
+import EditProfile from '@/components/Molecules/EditProfile/index.vue'
 import { axios } from '@/services/axios'
-import { useAuthStore, useProfileStore } from '@/store'
+import { useProfileStore } from '@/store'
 import { SizeAvatarEnum } from '@/type'
-import { APP_API } from '@/apis'
 
 definePageMeta({
   middleware: 'auth',
@@ -12,6 +13,8 @@ definePageMeta({
 
 const route = useRoute()
 const profileStore = useProfileStore()
+
+let showEditProfile = $ref(false)
 
 const { data, pending: pendingTimeline } = await useLazyAsyncData<any>('profile', async () => {
   const res = await axios.get(APP_API.USER.detail, {
@@ -31,23 +34,29 @@ const profile = $computed(() => profileStore.profile)
 
 <template>
   <div v-if="!pendingTimeline" class="relative mb-[65px] text-sm">
-    <div class="relative h-[200px] md:h-[250px]">
-      <nuxt-img
-        class="absolute h-full w-full cursor-pointer bg-cover bg-center object-cover"
-        :src="profile?.cover_pic_url"
-        :style="{ backgroundImage: `url(${profile?.cover_pic_url})` }"
-      />
+    <div class="relative">
+      <div class="group relative h-[200px] md:h-[250px]">
+        <nuxt-img
+          class="absolute h-full w-full cursor-pointer bg-cover bg-center object-cover"
+          :src="profile?.cover_pic_url"
+          :style="{ backgroundImage: `url(${profile?.cover_pic_url})` }"
+        />
+        <div class="absolute inset-0 hidden bg-black/50 group-hover:block"></div>
+      </div>
 
-      <Avatar
-        :size="SizeAvatarEnum.L"
-        :url="profile?.profile_pic_url"
-        class="absolute top-[67%] left-1/2 -translate-x-1/2 border-[5px] border-c1 dark:border-c19 md:top-[65%] md:left-[3%] md:h-[140px] md:w-[140px] md:translate-x-0"
-      />
+      <div class="group absolute h-[140px] w-[140px] -translate-y-[100px] translate-x-[20px]">
+        <Avatar
+          :size="SizeAvatarEnum.L"
+          :url="profile?.profile_pic_url"
+          class="absolute border-[5px] border-c1 dark:border-c19 md:h-full md:w-full"
+        />
+        <div class="absolute inset-0 hidden rounded-[50%] bg-black/50 group-hover:block"></div>
+      </div>
     </div>
     <div>
-      <div class="mt-[50px] flex flex-col items-center md:ml-[180px] md:mt-[5px] md:block">
+      <div class="flex flex-col items-center md:ml-[180px] md:mt-[15px] md:block">
         <TagName :name="profile?.user_name" />
-        <div class="flex space-x-[20px]"></div>
+        <AtomsButton text="Edit Profile" @click="showEditProfile = true" />
       </div>
     </div>
     <div class="mt-[20px] flex flex-col items-center justify-center">
@@ -56,5 +65,6 @@ const profile = $computed(() => profileStore.profile)
     </div>
     <div class="mt-[20px] flex justify-around"></div>
     <div class="mt-[20px] grid grid-cols-3 gap-[5px]"></div>
+    <EditProfile v-if="showEditProfile" @close="showEditProfile = false" />
   </div>
 </template>
