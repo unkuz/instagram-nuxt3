42ab87c2d96410851b51fb726a1658d66bffa12c
cuzknothz <cuzknothz@gmail.com>
2023-04-03 12:38:27 +0700

    🥕

--- components/Molecules/Search.vue
+++ components/Molecules/Search.vue
@@ -2,7 +2,12 @@
 import SearchIcon from '@/assets/svg/search_icon_2367fdg.svg'
 import { onClickOutside } from '@vueuse/core'
 
-const props = defineProps<{ value: string; isFocus: boolean }>()
+interface IProps {
+  value: string
+  isFocus: boolean
+}
+
+const props = defineProps<IProps>()
 
 const emit = defineEmits(['update:value', 'update:is-focus'])
 
@@ -10,16 +15,13 @@ const changeVal = (e: Event) => {
   emit('update:value', (e.target as HTMLInputElement).value)
 }
 
-let isFocus = $ref(false)
-
 const searchRef = ref<HTMLInputElement>()
 const inputRef = $ref<HTMLInputElement>()
-const isSearchHaveValue = $computed(() => !!props.value)
 
 watch(
-  () => isFocus,
-  (state) => {
-    if (state) inputRef?.focus()
+  () => props.isFocus,
+  (val) => {
+    if (val) inputRef?.focus()
   }
 )
 
@@ -34,7 +36,8 @@ onClickOutside(searchRef, () => updateIsFocus(false))
     class="relative flex cursor-text items-center rounded-md bg-c4/50 px-[16px] dark:bg-c23/50"
     @click="updateIsFocus(true)"
   >
-    <SearchIcon v-show="!isFocus" class="[&>path]:stroke-black [&>path]:dark:stroke-white" />
+    <SearchIcon class="[&>path]:stroke-black [&>path]:dark:stroke-white" />
+
     <input
       ref="inputRef"
       :value="value"
@@ -42,10 +45,7 @@ onClickOutside(searchRef, () => updateIsFocus(false))
       type="text"
       placeholder="Search"
       :class="[
-        'absolute left-1/2 top-1/2 h-[30px]  w-[90%] -translate-x-1/2 -translate-y-1/2 !bg-red-300/20 text-sm caret-c7 placeholder:pl-[25px] focus:outline-none',
-        {
-          'w-[calc(90% -22px)] ml-[22px]': value,
-        },
+        'absolute left-[40px] top-1/2 h-[30px] w-[calc(100%-40px-16px)] -translate-y-1/2 bg-transparent text-sm caret-c7 placeholder:pl-[25px] focus:outline-none',
       ]"
     />
   </div>
