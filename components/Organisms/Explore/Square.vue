ce9710b898903d6b99dd8ebc36eebdefa966c225
cuzknothz <cuzknothz@gmail.com>
2023-04-08 02:33:26 +0700

    🌵

--- components/Organisms/Explore/Square.vue
+++ components/Organisms/Explore/Square.vue
@@ -4,6 +4,7 @@ import { APP_CONFIGS } from '@/configs'
 import HeartIcon_ from '@/assets/svg/mingcute/heart.svg'
 import ChatIcon_ from '@/assets/svg/mingcute/chat.svg'
 import { isImage, isVideo } from '@/utils'
+import ReelIconSelected_ from '@/assets/svg/reel_icon_selected.svg'
 
 interface IProps {
   src: string
@@ -34,11 +35,15 @@ withDefaults(defineProps<IProps>(), { isBig: false, img: '', skeleton: false })
         :quality="APP_CONFIGS.QUALITY_IMAGE.EXPLORE"
       />
 
-      <video
-        v-if="isVideo(src)"
-        :src="src"
-        class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]"
-      />
+      <template v-if="isVideo(src)"
+        ><video
+          :src="src"
+          class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]" />
+        <ReelIconSelected_
+          :class="[
+            'absolute right-[5px] top-[5px] z-10 fill-c1 dark:fill-c1 md:right-[20px] md:top-[20px]',
+          ]"
+      /></template>
 
       <div
         class="absolute inset-0 hidden items-center justify-center bg-black/20 backdrop-blur-[5px] duration-200 group-hover:flex"
