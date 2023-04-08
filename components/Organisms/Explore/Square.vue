94cdf891d1773f78848810fdc93cb0d82dd70538
cuzknothz <cuzknothz@gmail.com>
2023-04-08 10:42:43 +0700

    🌵

--- components/Organisms/Explore/Square.vue
+++ components/Organisms/Explore/Square.vue
@@ -35,39 +35,35 @@ withDefaults(defineProps<IProps>(), {
       },
     ]"
   >
-    <NuxtLink v-if="!skeleton" :to="`/explore/${'1'}`">
-      <nuxt-img
-        v-if="isImage(src)"
-        :src="src"
-        :alt="src"
-        class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]"
-        :quality="APP_CONFIGS.QUALITY_IMAGE.EXPLORE"
-      />
+    <nuxt-img
+      v-if="isImage(src)"
+      :src="src"
+      :alt="src"
+      class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]"
+      :quality="APP_CONFIGS.QUALITY_IMAGE.EXPLORE"
+    />
 
-      <template v-if="isVideo(src)"
-        ><video
-          :src="src"
-          class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]" />
-        <ReelIconSelected_
-          :class="[
-            'absolute right-[5px] top-[5px] z-10 fill-c1 dark:fill-c1 md:right-[20px] md:top-[20px]',
-          ]"
-      /></template>
+    <template v-if="isVideo(src)"
+      ><video :src="src" class="h-full w-full object-cover duration-200 group-hover:scale-[1.1]" />
+      <ReelIconSelected_
+        :class="[
+          'absolute right-[5px] top-[5px] z-10 fill-c1 dark:fill-c1 md:right-[20px] md:top-[20px]',
+        ]"
+    /></template>
 
-      <div
-        class="absolute inset-0 hidden items-center justify-center bg-black/20 backdrop-blur-[5px] duration-200 group-hover:flex"
-      >
-        <div class="flex gap-[20px] text-c1 dark:text-c1 md:gap-[30px]">
-          <div class="flex flex-col items-center">
-            <HeartIcon_ />
-            <span>{{ $formatNum(likes) }}</span>
-          </div>
-          <div class="flex flex-col items-center">
-            <ChatIcon_ />
-            <span>{{ $formatNum(comments) }}</span>
-          </div>
+    <div
+      class="absolute inset-0 hidden items-center justify-center bg-black/20 backdrop-blur-[5px] duration-200 group-hover:flex"
+    >
+      <div class="flex gap-[20px] text-c1 dark:text-c1 md:gap-[30px]">
+        <div class="flex flex-col items-center">
+          <HeartIcon_ class="[&__path]:fill-c1" />
+          <span>{{ $formatNum(likes) }}</span>
+        </div>
+        <div class="flex flex-col items-center">
+          <ChatIcon_ class="[&__path]:fill-c1" />
+          <span>{{ $formatNum(comments) }}</span>
         </div>
       </div>
-    </NuxtLink>
+    </div>
   </div>
 </template>
