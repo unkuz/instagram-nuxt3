75430892618c2a84c457d0cdcc33416252c44789
cuzknothz <cuzknothz@gmail.com>
2023-04-10 00:58:08 +0700

    🍆

--- components/Organisms/Feed/List/Head.vue
+++ components/Organisms/Feed/List/Head.vue
@@ -5,6 +5,7 @@ import TagName from '@/components/Atoms/TagName.vue'
 import { useMoreStore } from '@/store'
 import { SizeAvatarEnum } from '@/type'
 import { TypeMoreEnum } from '@/store'
+import PreviewProfile from '@/components/Molecules/PreviewProfile.vue'
 
 const moreStore = useMoreStore()
 
@@ -16,11 +17,17 @@ interface IProps {
 
 const props = defineProps<IProps>()
 
+const isShowBriefProfile = $ref(false)
+
 const showMore = () => {
   moreStore.set({
     type: TypeMoreEnum.FEED,
     data: props.feed,
   })
+}
+
+const jeje = () => {
+  console.log('asdfhjsdjkf')
 }
 </script>
 
@@ -29,8 +36,24 @@ const showMore = () => {
     <div class="mx-[16px] flex h-full items-center justify-between">
       <div class="flex items-center space-x-[10px] text-[0.8rem]">
         <NuxtLink :to="`/${userName}/`">
-          <Avatar :size="SizeAvatarEnum.S" :url="avatar"
-        /></NuxtLink>
+          <div
+            class="relative"
+            @mouseover="isShowBriefProfile = true"
+            @mouseleave="isShowBriefProfile = false"
+          >
+            <Avatar :size="SizeAvatarEnum.S" :url="avatar" />
+            <div
+              :class="[
+                'absolute top-[40px] z-10 hidden duration-200',
+                {
+                  'block ': isShowBriefProfile,
+                },
+              ]"
+            >
+              <PreviewProfile v-bind="feed.user" />
+            </div>
+          </div>
+        </NuxtLink>
 
         <NuxtLink :to="`/${userName}/`"><TagName :name="userName" /></NuxtLink>
       </div>
