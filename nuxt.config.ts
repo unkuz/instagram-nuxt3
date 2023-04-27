6030530afe0209a33ee1373310307f8e9f46efe0
cuzknothz <cuzknothz@gmail.com>
2023-04-27 22:00:00 +0700

    🥕

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,13 +26,7 @@ export default defineNuxtConfig({
     // '@nuxtjs/fontaine',
     // '@morev/vue-transitions/nuxt',
   ],
-  //   auth: {
-  //     enableGlobalAppMiddleware: true,
-  //     origin: 'http://localhost:3000/',
-  //   },
-  content: {
-    // https://content.nuxtjs.org/api/configuration
-  },
+  content: {},
   extends: ['nuxt-seo-kit'],
   app: {
     head: {
@@ -85,16 +79,6 @@ export default defineNuxtConfig({
   },
   vite: {
     plugins: [svgLoader({}), VueTypeImports()],
-    // build: {
-    //   rollupOptions: {
-    //     output: {
-    //       manualChunks(id) {
-    //         const seprateModule = firebaseModules.find((module) => id.includes(module))
-    //         if (seprateModule) return seprateModule
-    //       },
-    //     },
-    //   },
-    // },
     build: {
       chunkSizeWarningLimit: 1600,
     },
@@ -111,9 +95,9 @@ export default defineNuxtConfig({
     upperAfterPrefix: false,
     exclude: ['map'],
     alias: [
-      ['camelCase', 'stringToCamelCase'], // => stringToCamelCase
-      ['kebabCase', 'stringToKebab'], // => stringToKebab
-      ['isDate', 'isLodashDate'], // => _isLodashDate
+      ['camelCase', 'stringToCamelCase'],
+      ['kebabCase', 'stringToKebab'],
+      ['isDate', 'isLodashDate'],
     ],
   },
   experimental: {
@@ -124,25 +108,6 @@ export default defineNuxtConfig({
   devtools: {
     enabled: true,
   },
-  // auth: {
-  //     // The module is enabled. Change this to disable the module
-  //     isEnabled: true,
-  //     // The origin is set to the development origin. Change this when deploying to production by setting `origin` in this config before build-time or by exporting `AUTH_ORIGIN` by running `export AUTH_ORIGIN=...`
-  //     origin: 'http://localhost:3000',
-  //     // The base path to the authentication endpoints. Change this if you want to add your auth-endpoints at a non-default location
-  //     basePath: '/api/auth',
-  //     // Whether to periodically refresh the session. Change this to `true` for a refresh every seconds or set this to a number like `5000` for a refresh every 5000 milliseconds (aka: 5 seconds)
-  //     enableSessionRefreshPeriodically: false,
-  //     // Whether to refresh the session whenever a window focus event happens, i.e, when your user refocuses the window. Set this to `false` to turn this off
-  //     enableSessionRefreshOnWindowFocus: true,
-  //     // Whether to add a global authentication middleware that will protect all pages without exclusion
-  //     enableGlobalAppMiddleware: false,
-  //     // Configuration of the global auth-middleware (only applies if you set `enableGlobalAppMiddleware: true` above!)
-  //     globalMiddlewareOptions: {
-  //         // Whether to allow access to 404 pages without authentication. Set this to `false` to force users to sign-in before seeing `404` pages. Setting this to false may lead to vue-router problems (as the target page does not exist)
-  //         allow404WithoutAuth: true,
-  //     },
-  // },
   runtimeConfig: {
     public: {
       siteUrl: 'https://instagram-clone-nuxt3.vercel.app/',
